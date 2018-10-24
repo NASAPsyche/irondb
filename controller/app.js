51ae2bfad972eaae7d5f18712ae5d5e6ce927fce
Troy Mullins <Troy.Mullins@asu.edu>
2018-10-24 16:59:03 -0400

    Task 117: Added EJS and an example template.

--- controller/app.js
+++ controller/app.js
@@ -8,6 +8,9 @@ var indexRouter = require('./routes/index');
 
 var app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, '../views'));
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
--- controller/routes/index.js
+++ controller/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.json({ title: 'Iron Meteorite Example' });
+  res.render('index', { title: 'Iron Meteorite Example' });
 });
 
 module.exports = router;
