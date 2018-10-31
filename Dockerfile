7b3370903738b1e8bd07d10cec1a8a90d6979b64
Troy Mullins <Troy.Mullins@asu.edu>
2018-10-31 05:13:30 -0400

    Task 135: Added node-postgres and example route pulling data from db and displaying it in the browser.

--- Dockerfile
+++ Dockerfile
@@ -3,12 +3,11 @@ FROM node:8.12.0
 # Move working directory to default build location.
 WORKDIR /usr/app
 
-# Copying package.json and installing dependencies.
-COPY package.json .
-RUN npm install --quiet
-
 # Copying directory into container.
 COPY . .
+
+# Copying package.json and installing dependencies.
+RUN npm install --quiet
 
 # Installing gulp globally, then running tasks.
 RUN npm install --global gulp-cli
---
+++ controller/routes/database.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+
+const { Client } = require('pg');
+const client = new Client({
+	connectionString: 'postgres://group16:abc123@pgdb:5432/postgres'
+});
+
+
+// Connect to database, should use env variable set in docker-compose
+client.connect();
+
+var query_response = "";
+client.query('SELECT * FROM Entries', (err, result) => {
+	// Store response
+	if (!err) {
+		query_response = result;
+	}
+
+	// Log Error
+	console.log(err ? err.stack : result.rows[0].message);
+
+	// Close connection with database
+	client.end((err) => {
+		if (err) {
+			console.log('Error during disconnection.', err.stack);
+		} else {
+			console.log('Disconnected from DB.');
+		}
+	})
+});
+
+/* GET database page. */
+router.get('/', function(req, res, next) {
+  res.render('database', { id: query_response.rows[0].entry_id, name: query_response.rows[0].name });
+});
+
+
+
+module.exports = router;
