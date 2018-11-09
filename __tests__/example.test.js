98a22da63859910920463c9eca900fd50a814b8c
Troy Mullins <Troy.Mullins@asu.edu>
2018-11-09 12:27:37 -0500

    Task 255: Write tests for example route walkthrough part 1.
    Former-commit-id: ecbf1bf70c9cb587dd945f194a81d188e7842bda

---
+++ __tests__/example.test.js
@@ -0,0 +1,12 @@
+const request = require('supertest');
+const app = require('../controller/app');
+
+describe('Test the example path', () => {
+    test('It should respond to the GET method with example.ejs template and 200 status.', (done) => {
+        request(app).get('/example').then((response) => {
+            expect(response.statusCode).toBe(200);
+            expect(response.text).toMatch(/Example Route/);
+            done();
+        });
+    });
+});
