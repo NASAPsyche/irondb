9de200e881520e30d44b587c5048c5631a0282ad
Michael Falgien <mfalgien@gmail.com>
2018-11-09 18:20:34 -0800

    added functionality to determine the classification of the input and return the classification of the element
    Former-commit-id: 0879f9c4b904495bdddbf35211e2817dcee5cf5c

--- imdb_papers/Classification/Classification.py
+++ imdb_papers/Classification/Classification.py
@@ -1,14 +1,67 @@
 #This is my first python program, don't judge especially you Hajar
 print("Please enter a number first units will come later")
 
-x = int(input())
-print("You entered: %d" % x)
+x = float(input())
+valid = False
+
+#get input from user
+print("You entered: %f" % x)
 print("Enter the units: ")
 units = input()
 
-unitList = ["ng/g", "mg/g", "ug/g"]
+#known measurements
+unitList = ["ng/g", "mg/g", "ug/g", "ppm", "ppb"]
 
 if units in unitList:
-    print("yes")
+    print("valid unit")
+    valid = True
 else:
-    print("no")
+    print("not valid unit")
+
+#convert from ng/g to ppm
+def convert_nanograms(val):
+    x = val / 1000
+    return x
+
+#convert from mg/g to ppm
+def convert_milligram(val):
+    x = val * 1000
+    return x
+
+#convert from ug/g to ppm
+def convert_microgram(val):
+    x = val
+    return x
+
+#convert from ppb to ppm
+def convert_ppb(val):
+    x = val / 1000
+    return x
+
+#convert input to units
+if (valid):
+    if units == "ng/g":
+        ppm = convert_nanograms(x)
+        print("%f ppm" % ppm)
+    elif units == "mg/g":
+        ppm = convert_milligram(x)
+        print("%f ppm" % ppm)
+    elif units == "ug/g":
+        ppm = convert_microgram(x)
+        print("%f ppm" % ppm)
+else:
+    print("The units were not valid and could not be converted, loser")
+
+#determine the classification of the element
+def _classification(ppm):
+    if(ppm > 10000):
+        classification = "Major"
+    elif(ppm >= 100 and ppm < 10000):
+        classification = "Minor"
+    elif(ppm < 100):
+        classification = "Trace"
+    return print("The element is a %s element" % classification)
+
+#print results
+_classification(ppm)
+
