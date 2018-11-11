f9e377b0855f3792797142644759d0a2f2baedfa
Troy Mullins <Troy.Mullins@asu.edu>
2018-11-11 10:50:29 -0500

    Task 164: Refactored example db route to use new pattern.
    Former-commit-id: 56644aacd8e7c76cd8ef8f0052f2e04b3fe4afdb

--- controller/app.js
+++ controller/app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-//var databaseRouter = require('./routes/database');
+var databaseRouter = require('./routes/database');
 
 var app = express();
 
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/', indexRouter);
-//app.use('/database', databaseRouter);
+app.use('/database', databaseRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
