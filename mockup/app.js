a4e45cd99667feb08c50793c003b5de479cbed1d
Ken Bonilla <kenbonilla@gmail.com>
2018-11-12 12:56:44 -0600

    Made three themes, set up routes

--- mockup/app.js
+++ mockup/app.js
@@ -5,7 +5,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var usersRouter = require('./routes/users');
+var lightRouter = require('./routes/light');
+var whiteRouter = require('./routes/white');
 
 var app = express();
 
@@ -20,7 +22,15 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/light', lightRouter);
+app.use('/white', whiteRouter);
+// GET method route
+// Landing page
+// app.get('/light', (req, res) => {
+//   app.render('light.ejs', (err, renderedData) => {
+//     res.send(renderedData);
+//   })
+// })
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ mockup/routes/white.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET light page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Psyche', sheet: "custom-white" });
+});
+
+module.exports = router;
