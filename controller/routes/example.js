c8c06a56eb7e65d4f84faaf743ee342f2a55797f
Troy Mullins <Troy.Mullins@asu.edu>
2018-11-15 11:51:17 -0500

    Tasks 209 and 252: Added examples of database interaction and protected routing.
    Former-commit-id: 0fc379d7790e68255baff62230efd91ab150fc42

--- controller/routes/example.js
+++ controller/routes/example.js
@@ -1,15 +1,29 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const db = require('../db');
+const { isLoggedIn, isAdmin } = require('../middleware/auth');
 
 /* GET example page. */
 router.get('/', function(req, res, next) {
   if (req.xhr === true) {
     // If ajax request return information.
-    res.json({username: "user1"});
+    db.query('SELECT username FROM Users', [], (dbErr, dbRes) => {
+    if (dbErr) {
+      return next(dbErr);
+    }
+      res.json({usernames: dbRes.rows});
+    });
+
+
   } else {
   	// If not ajax handel as regular get request.
   	res.render('example', { title: 'Example Route' });
   }
 });
 
+router.get('/secret', isLoggedIn, function(req, res, next) {
+	res.send("super secret information.");
+});
+
 module.exports = router;
+
--- public/javascripts/example.js
+++ public/javascripts/example.js
@@ -2,6 +2,15 @@ $("button").on("click", function(){
 	// AJAX to call the server to get all usernames.
 	$.get("/example", function(data){
 		// Update the page to include that information.
-		$("#update-target").text(data.username);
+		var usernames = data["usernames"];
+		console.log(usernames);
+		var i = 1;
+		usernames.forEach((entry) => {
+			$("#update-target").after("<p> Username " + i + ": " + entry.username + "</p><br>");
+			i++;
+		});
 	});
 });
+
+
+// {"usernames":[{"username":"bob"}]}
