de69fa280a273d331bb1bdc598cff3bd99649036
kenbonilla@gmail.com <kenbonilla@gmail.com>
2018-11-17 14:13:43 -0600

    US165 T166 US168 T167 - CREATE BACKUP/RESTORE DB METHOD From iron.sh select 1 -  to make a brand new container instance then select d - to enter database management select b - makes a backup // or // select r - to restore from most recent backup
    Added initialization of tables described by schema from US168 T287

--- iron.sh
+++ iron.sh
@@ -5,6 +5,7 @@ echo "2 - Rebuild containers and launch"
 echo "3 - Launch pre-built containers"
 echo "4 - Stop the containers"
 echo "9 - Stop and Remove ALL containers"
+echo "d - Database management (backup/restore)"
 echo "x - Exit the program"
 
 # read choice 
@@ -32,6 +33,24 @@ then
 elif [ $choice == 'x' ]
 then
     echo "Goodbye"
+elif [ $choice == 'd' ]
+then
+    echo "Postgress must be running for these operations"
+    echo "b - Make a backup"
+    echo "r - Restore from most recent backup"
+    echo -n "Enter selection : " ; read choice 
+    if [ $choice == 'b' ]
+    then
+        docker exec -t postgres pg_dump -c -U group16 -d postgres > backup-pg/pg_`date +%d-%m-%Y"_"%H_%M_%S`.sql
+    elif [ $choice == 'r' ]
+    then
+        cd backup-pg
+        STR="$(ls -tr | tail -1)"
+        echo "Restoring from $STR"
+        sleep 1
+        cat $STR | docker exec -i postgres psql -U group16 -d postgres
+        cd ..
+    fi
 else
     echo "Invalid selection"
 fi
