071058c18710e7b357f911fb5eaae12ee7e6dc2e
Troy Mullins <Troy.Mullins@asu.edu>
2018-11-23 20:04:48 -0500

    Tasks 300/301: Added sql inserts for example data.
    Former-commit-id: 9206426f8f65056f1290b6d4839c6ead288f00f7

--- db-init/init.sql
+++ db-init/init.sql
@@ -27,7 +27,6 @@ CREATE TABLE IF NOT EXISTS Papers (
 
 CREATE TABLE IF NOT EXISTS Entries (
 	entry_id serial PRIMARY KEY,
-	journal_id integer REFERENCES Journals(journal_id),
 	paper_id integer REFERENCES Papers(paper_id),
 	meteorite_name text NOT NULL,
 	page_number integer NOT NULL
@@ -53,21 +52,21 @@ CREATE TABLE IF NOT EXISTS Groups (
 CREATE TABLE IF NOT EXISTS Major_elements (
   entry_id integer REFERENCES Entries(entry_id),
   element varchar(10) NOT NULL,
-  measurement integer NOT NULL,
+  measurement decimal NOT NULL,
   PRIMARY KEY(entry_id, element)
 );
 
 CREATE TABLE IF NOT EXISTS Minor_elements (
   entry_id integer REFERENCES Entries(entry_id),
   element varchar(10) NOT NULL,
-  measurement integer NOT NULL,
+  measurement decimal NOT NULL,
   PRIMARY KEY(entry_id, element)
 );
 
 CREATE TABLE IF NOT EXISTS Trace_elements (
   entry_id integer REFERENCES Entries(entry_id),
   element varchar(10) NOT NULL,
-  measurement integer NOT NULL,
+  measurement decimal NOT NULL,
   PRIMARY KEY(entry_id, element)
 );
 
@@ -78,11 +77,317 @@ CREATE TABLE IF NOT EXISTS Notes (
 
 CREATE TABLE IF NOT EXISTS Entry_status (
   entry_id integer REFERENCES Entries(entry_id),
-  insert_date date,
-  inserted_by varchar(25) REFERENCES Users(username),
+  insert_date date DEFAULT current_date,
+  inserted_by varchar(25),
   checked_by varchar(25),
   status varchar(10) CHECK (
     status LIKE 'active' OR status LIKE 'inactive' OR status LIKE 'unapproved' OR status LIKE 'reported')
 );
 
--- Insert example entry
+
+
+-- Insert example data
+INSERT INTO Journals (journal_name, issue_number, published_year) 
+  VALUES ('Geochimica et Cosmochimica Acta', 'Vol. 44', 1980);
+
+INSERT INTO Papers (journal_id, title)
+  SELECT journal_id, 'Chemical classification of iron meteorites-IX. A new group (IIF), revision of IAB and IIICD, and data on 57 additional irons'
+  FROM Journals
+  WHERE journal_name='Geochimica et Cosmochimica Acta' AND issue_number='Vol. 44';
+
+INSERT INTO Authors (paper_id, author)
+  SELECT paper_id, 'ALFRED KRACHER'
+  FROM Papers
+  WHERE title='Chemical classification of iron meteorites-IX. A new group (IIF), revision of IAB and IIICD, and data on 57 additional irons';
+
+INSERT INTO Authors (paper_id, author)
+  SELECT paper_id, 'JOHN WILLIS'
+  FROM Papers
+  WHERE title='Chemical classification of iron meteorites-IX. A new group (IIF), revision of IAB and IIICD, and data on 57 additional irons';
+
+INSERT INTO Authors (paper_id, author)
+  SELECT paper_id, 'JOHN T. WASSON'
+  FROM Papers
+  WHERE title='Chemical classification of iron meteorites-IX. A new group (IIF), revision of IAB and IIICD, and data on 57 additional irons';
+
+INSERT INTO Entries (paper_id, meteorite_name, page_number)
+  SELECT paper_id, 'Alt Bela', 774
+  FROM Papers
+  WHERE title='Chemical classification of iron meteorites-IX. A new group (IIF), revision of IAB and IIICD, and data on 57 additional irons';
+
+INSERT INTO Entries (paper_id, meteorite_name, page_number)
+  SELECT paper_id, 'Carver', 774
+  FROM Papers
+  WHERE title='Chemical classification of iron meteorites-IX. A new group (IIF), revision of IAB and IIICD, and data on 57 additional irons';
+
+INSERT INTO Entries (paper_id, meteorite_name, page_number)
+  SELECT paper_id, 'Old Woman', 774
+  FROM Papers
+  WHERE title='Chemical classification of iron meteorites-IX. A new group (IIF), revision of IAB and IIICD, and data on 57 additional irons';
+
+INSERT INTO Analysis_technique (entry_id, technique)
+  SELECT entry_id, 'INAA'
+  FROM Entries
+  WHERE meteorite_name='Alt Bela';
+
+INSERT INTO Analysis_technique (entry_id, technique)
+  SELECT entry_id, 'INAA'
+  FROM Entries
+  WHERE meteorite_name='Carver';
+
+INSERT INTO Analysis_technique (entry_id, technique)
+  SELECT entry_id, 'INAA'
+  FROM Entries
+  WHERE meteorite_name='Old Woman';
+
+INSERT INTO Groups (entry_id, classification_group)
+  SELECT entry_id, 'IID'
+  FROM Entries
+  WHERE meteorite_name='Alt Bela';
+
+INSERT INTO Groups (entry_id, classification_group)
+  SELECT entry_id, 'IIA'
+  FROM Entries
+  WHERE meteorite_name='Carver';
+
+INSERT INTO Groups (entry_id, classification_group)
+  SELECT entry_id, 'IIB'
+  FROM Entries
+  WHERE meteorite_name='Old Woman';
+
+-- All measurments in wt.%, Major > 1%, Minor Between 1% and 0.1%, Trace < 0.1%
+INSERT INTO Major_elements (entry_id, element, measurement)
+  SELECT entry_id, 'Ni', 10.04
+  FROM Entries
+  WHERE meteorite_name='Alt Bela';
+
+INSERT INTO Major_elements (entry_id, element, measurement)
+  SELECT entry_id, 'Ni', 5.5
+  FROM Entries
+  WHERE meteorite_name='Carver';
+
+INSERT INTO Major_elements (entry_id, element, measurement)
+  SELECT entry_id, 'Ni', 5.71
+  FROM Entries
+  WHERE meteorite_name='Old Woman';
+
+INSERT INTO Trace_elements (entry_id, element, measurement)
+  SELECT entry_id, 'Ga', 0.0075
+  FROM Entries
+  WHERE meteorite_name='Alt Bela';
+
+INSERT INTO Trace_elements (entry_id, element, measurement)
+  SELECT entry_id, 'Ga', 0.00587
+  FROM Entries
+  WHERE meteorite_name='Carver';
+
+INSERT INTO Trace_elements (entry_id, element, measurement)
+  SELECT entry_id, 'Ga', 0.00585
+  FROM Entries
+  WHERE meteorite_name='Old Woman';
+
+INSERT INTO Trace_elements (entry_id, element, measurement)
+  SELECT entry_id, 'Ge', 0.0084
+  FROM Entries
+  WHERE meteorite_name='Alt Bela';
+
+INSERT INTO Trace_elements (entry_id, element, measurement)
+  SELECT entry_id, 'Ge', 0.0184
+  FROM Entries
+  WHERE meteorite_name='Carver';
+
+INSERT INTO Trace_elements (entry_id, element, measurement)
+  SELECT entry_id, 'Ge', 0.019
+  FROM Entries
+  WHERE meteorite_name='Old Woman';
+
+INSERT INTO Trace_elements (entry_id, element, measurement)
+  SELECT entry_id, 'Ir', 0.0016
+  FROM Entries
+  WHERE meteorite_name='Alt Bela';
+
+INSERT INTO Trace_elements (entry_id, element, measurement)
+  SELECT entry_id, 'Ir', 0.0012
+  FROM Entries
+  WHERE meteorite_name='Carver';
+
+INSERT INTO Trace_elements (entry_id, element, measurement)
+  SELECT entry_id, 'Ir', 0.00008
+  FROM Entries
+  WHERE meteorite_name='Old Woman';
+
+INSERT INTO Entry_status (entry_id, status)
+  SELECT entry_id, 'active'
+  FROM Entries
+  WHERE meteorite_name='Alt Bela';
+
+INSERT INTO Entry_status (entry_id, status)
+  SELECT entry_id, 'active'
+  FROM Entries
+  WHERE meteorite_name='Carver';
+
+INSERT INTO Entry_status (entry_id, status)
+  SELECT entry_id, 'active'
+  FROM Entries
+  WHERE meteorite_name='Old Woman';
+
+
+
+-- More example data from different paper
+INSERT INTO Journals (journal_name, issue_number, published_year) 
+  VALUES ('Geochimica et Cosmochimica Acta', '197', 2017);
+
+INSERT INTO Papers (journal_id, title)
+  SELECT journal_id, 'Formation of non-magmatic iron-meteorite group IIE'
+  FROM Journals
+  WHERE journal_name='Geochimica et Cosmochimica Acta' AND issue_number='197';
+
+INSERT INTO Authors (paper_id, author)
+  SELECT paper_id, 'JOHN T. WASSON'
+  FROM Papers
+  WHERE title='Formation of non-magmatic iron-meteorite group IIE';
+
+INSERT INTO Entries (paper_id, meteorite_name, page_number)
+  SELECT paper_id, 'Leshan', 399
+  FROM Papers
+  WHERE title='Formation of non-magmatic iron-meteorite group IIE';
+
+INSERT INTO Entries (paper_id, meteorite_name, page_number)
+  SELECT paper_id, 'Tobychan', 399
+  FROM Papers
+  WHERE title='Formation of non-magmatic iron-meteorite group IIE';
+
+INSERT INTO Analysis_technique (entry_id, technique)
+  SELECT entry_id, 'INAA'
+  FROM Entries
+  WHERE meteorite_name='Leshan';
+
+INSERT INTO Analysis_technique (entry_id, technique)
+  SELECT entry_id, 'INAA'
+  FROM Entries
+  WHERE meteorite_name='Tobychan';
+
+INSERT INTO Groups (entry_id, classification_group)
+  SELECT entry_id, 'IIE'
+  FROM Entries
+  WHERE meteorite_name='Leshan';
+
+INSERT INTO Groups (entry_id, classification_group)
+  SELECT entry_id, 'IIE'
+  FROM Entries
+  WHERE meteorite_name='Tobychan';
+
+INSERT INTO Major_elements (entry_id, element, measurement)
+  SELECT entry_id, 'Ni', 9.67
+  FROM Entries
+  WHERE meteorite_name='Leshan';
+
+INSERT INTO Major_elements (entry_id, element, measurement)
+  SELECT entry_id, 'Ni', 7.56
+  FROM Entries
+  WHERE meteorite_name='Tobychan';
+
+INSERT INTO Minor_elements (entry_id, element, measurement)
+  SELECT entry_id, 'Co', 0.451
+  FROM Entries
+  WHERE meteorite_name='Leshan';
+
+INSERT INTO Minor_elements (entry_id, element, measurement)
+  SELECT entry_id, 'Co', 0.436
+  FROM Entries
+  WHERE meteorite_name='Tobychan';
+
+INSERT INTO Trace_elements (entry_id, element, measurement)
+  SELECT entry_id, 'Cr', 0.0014
+  FROM Entries
+  WHERE meteorite_name='Leshan';
+
+INSERT INTO Trace_elements (entry_id, element, measurement)
+  SELECT entry_id, 'Cr', 0.0012
+  FROM Entries
+  WHERE meteorite_name='Tobychan';
+
+INSERT INTO Trace_elements (entry_id, element, measurement)
+  SELECT entry_id, 'Cu', 0.0326
+  FROM Entries
+  WHERE meteorite_name='Leshan';
+
+INSERT INTO Trace_elements (entry_id, element, measurement)
+  SELECT entry_id, 'Cu', 0.0174
+  FROM Entries
+  WHERE meteorite_name='Tobychan';
+
+INSERT INTO Trace_elements (entry_id, element, measurement)
+  SELECT entry_id, 'Ga', 0.002
+  FROM Entries
+  WHERE meteorite_name='Leshan';
+
+INSERT INTO Trace_elements (entry_id, element, measurement)
+  SELECT entry_id, 'Ga', 0.00274
+  FROM Entries
+  WHERE meteorite_name='Tobychan';
+
+INSERT INTO Trace_elements (entry_id, element, measurement)
+  SELECT entry_id, 'Ge', 0.00689
+  FROM Entries
+  WHERE meteorite_name='Leshan';
+
+INSERT INTO Trace_elements (entry_id, element, measurement)
+  SELECT entry_id, 'Ge', 0.00749
+  FROM Entries
+  WHERE meteorite_name='Tobychan';
+
+INSERT INTO Trace_elements (entry_id, element, measurement)
+  SELECT entry_id, 'As', 0.00177
+  FROM Entries
+  WHERE meteorite_name='Leshan';
+
+INSERT INTO Trace_elements (entry_id, element, measurement)
+  SELECT entry_id, 'As', 0.00074
+  FROM Entries
+  WHERE meteorite_name='Tobychan';
+
+INSERT INTO Trace_elements (entry_id, element, measurement)
+  SELECT entry_id, 'Sb', 0.000049
+  FROM Entries
+  WHERE meteorite_name='Leshan';
+
+INSERT INTO Trace_elements (entry_id, element, measurement)
+  SELECT entry_id, 'W', 0.000119
+  FROM Entries
+  WHERE meteorite_name='Leshan';
+
+INSERT INTO Trace_elements (entry_id, element, measurement)
+  SELECT entry_id, 'W', 0.000122
+  FROM Entries
+  WHERE meteorite_name='Tobychan';
+
+INSERT INTO Trace_elements (entry_id, element, measurement)
+  SELECT entry_id, 'Re', 0.0000484
+  FROM Entries
+  WHERE meteorite_name='Leshan';
+
+INSERT INTO Trace_elements (entry_id, element, measurement)
+  SELECT entry_id, 'Re', 0.0000650
+  FROM Entries
+  WHERE meteorite_name='Tobychan';
+
+INSERT INTO Trace_elements (entry_id, element, measurement)
+  SELECT entry_id, 'Ir', 0.000429
+  FROM Entries
+  WHERE meteorite_name='Leshan';
+
+INSERT INTO Trace_elements (entry_id, element, measurement)
+  SELECT entry_id, 'Ir', 0.000653
+  FROM Entries
+  WHERE meteorite_name='Tobychan';
+
+INSERT INTO Entry_status (entry_id, status)
+  SELECT entry_id, 'active'
+  FROM Entries
+  WHERE meteorite_name='Leshan';
+
+INSERT INTO Entry_status (entry_id, status)
+  SELECT entry_id, 'active'
+  FROM Entries
+  WHERE meteorite_name='Tobychan';
