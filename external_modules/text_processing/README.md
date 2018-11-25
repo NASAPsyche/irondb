04800afcb7a26062385daa4ed7d8939a2c9e5860
Hajar <hboughou@asu.edu>
2018-11-25 14:58:10 -0700

    removing text_processing directory
    Former-commit-id: 9d8d9f7bcd024cb1bcbc4babb71bb644e0927c5d

--- external_modules/text_processing/README.md
+++
@@ -1,6 +0,0 @@
-﻿### To Use
- 
-1. Make sure the script is in the same directory as pdf2txt 
-2. Include your test pdf in the same directory
-3. Run inside your IDE and follow user prompts
-
--- external_modules/text_processing/metadata_manual_extract.py
+++
@@ -1,9 +0,0 @@
-import pdf2txt
-
-input_pdf = input('Enter file name: ')
-pdf2txt.main([input_pdf, '-p 1', '-o firstpage.txt'])
-
-#split at Abstract to get text before that
-#with open('firstpage.txt', 'r') as myfile:  #can't call file where it's generated
-    #relevant_data = myfile.read().replace('\n', '')
-#relevant_data.split("Abstract",1)[1]
