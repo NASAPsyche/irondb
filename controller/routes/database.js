7fe5110a36d3082c53e9615be6debbab270a9f90
Troy Mullins <Troy.Mullins@asu.edu>
2018-11-26 01:13:39 -0500

    Task 196: Added logic to build query based on provided params.
    Former-commit-id: 7d1bf02864ad01f39df8fe458741f5b9b51ca732

--- controller/routes/database.js
+++ controller/routes/database.js
@@ -4,15 +4,75 @@ const db = require('../db');
 
 /* GET database page. */
 router.get('/', function(req, res, next) {
-  var entriesTable = [];
   db.query('SELECT * FROM complete_table WHERE status=$1', ['active'], (dbErr, dbRes) => {
     if (dbErr) {
       return next(dbErr);
     }
-
-    entriesTable = dbRes.rows;
     res.render('database', { Entries: dbRes.rows });
   });
+});
+
+/* POST database page */
+router.post('/', function(req, res, next) {
+  if (req.xhr) {
+  	res.send("xhr detected.");
+  } else {
+	//res.send(req.body);
+
+	var queryString = "SELECT * FROM complete_table WHERE status=$1 ";
+	var argsArray = ['active'];
+	var currentQueryIndex = 2;
+
+	if (req.body.name !== "") {
+		argsArray.push(req.body.name);
+		queryString += ("AND meteorite_name ~* $" + currentQueryIndex + " ");
+		currentQueryIndex++;
+	}
+
+	if (req.body.title !== "") {
+		argsArray.push(req.body.title);
+		queryString += ("AND title ~* $" + currentQueryIndex + " ");
+		currentQueryIndex++;
+	}
+
+	if (req.body.author !== "") {
+		argsArray.push(req.body.author);
+		queryString += ("AND authors ~* $" + currentQueryIndex + " ");
+		currentQueryIndex++;
+	}
+
+	if (req.body.group !== "Group") {
+		argsArray.push(req.body.group);
+		queryString += ("AND classification_group=$" + currentQueryIndex + " ");
+		currentQueryIndex++;
+	}
+
+	if (req.body.element !== "Element" && req.body.range !== "Range") {
+		argsArray.push(req.body.element);
+		
+		switch (req.body.range) {
+			case 'major':
+				queryString += ("AND major_elements ~* $" + currentQueryIndex + " ");
+				break;
+			case 'minor':
+				queryString += ("AND minor_elements ~* $" + currentQueryIndex + " ");
+				break;
+			case 'trace':
+				queryString += ("AND trace_elements ~* $" + currentQueryIndex + " ");
+				break;
+		}
+
+		currentQueryIndex++;
+	}
+
+
+	db.query(queryString, argsArray, (dbErr, dbRes) => {
+	    if (dbErr) {
+	      return next(dbErr);
+	    }
+	    res.render('database', { Entries: dbRes.rows });
+	});
+  }
 });
 
 
