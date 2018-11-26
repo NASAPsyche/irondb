cd8451d000729748ce2ba40664a77af5c0eb8b1a
Troy Mullins <Troy.Mullins@asu.edu>
2018-11-25 19:08:26 -0500

    Task 192: Added search ui.
    Former-commit-id: a8161a9fbaa4252d51d0ab44726eeda8907121c2

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ gulp.task('sass', function() {
 
 // Move JS files to public javascripts directory.
 gulp.task('js', function() {
-	return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/tether/dist/js/tether.min.js'])
+	return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/tether/dist/js/tether.min.js','node_modules/popper.js/dist/popper.min.js'])
 		.pipe(gulp.dest("public/javascripts"));
 });
 
