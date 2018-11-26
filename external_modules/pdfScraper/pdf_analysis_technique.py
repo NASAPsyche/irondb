141165a1eda728e30333758e06f5c77d6dc90888
Michael Falgien <mfalgien@gmail.com>
2018-11-25 21:30:59 -0800

    Initial attempt to find analysis techniques in WassonandChoe_GCA_2009

---
+++ external_modules/pdfScraper/pdf_analysis_technique.py
@@ -0,0 +1,67 @@
+"""
+driver.py: The purpose of this file is to develop the entire journal scraper functionality with the touch of a button. This
+is simply meant to help devlop python parts of the web application. This is not shipable code.
+
+__authors__ = "Joshua Johnson", "Michael Falgien"
+__version__ = "1.0"
+__email__ = "jdjohn43@asu.edu", "mfalgien@gmail.com"
+__date__ = "11/25/18"
+"""
+
+import pdf_metadata
+import pdf_tables
+import driver_methods
+import pdf_text
+import find_tables
+
+
+# START This is getting the current pdfs in the pdf folder.
+chosen_pdf = driver_methods.display_pdf_names("pdfs/")
+print("\n You selected: " + chosen_pdf)
+# END This is getting the current pdfs in the pdf folder.
+
+chosen_pdf = "pdfs/" + chosen_pdf
+
+# START Getting Metadata
+# pdf_metadata.get_metadata(chosen_pdf)
+# END Getting Metadata
+
+
+# START Get number of Pages
+total_pages = pdf_metadata.get_num_pages(chosen_pdf)
+# START Get number of Pages
+print("There are " + str(total_pages) + " pages in this document.")
+
+
+# START get text from pdf
+text = pdf_text.convert_pdf_to_txt_looper(chosen_pdf, total_pages)
+print("The following is the entire text from the chosen pdf. \n")
+# The next line will give you guys the first page of the pdf. You can grab the whole array called "text"
+# and do what you want with it.
+
+# Print the entire contents of text from pdf
+# for i in range(total_pages):
+#    print("Page #%i" % (i + 1))
+#    print(text[i])
+
+if 'INAA' in text[1]:
+    print("INAA Found")
+if 'RNAA' in text[1]:
+    print("RNAA Found")
+
+# print(text)
+# End get text from pdf.
+
+# START Getting pages that have tables on them.
+#pages_with_table = find_tables.look_for_tables(text, total_pages)
+# END Getting pages that have tables on them.
+
+#print(str(pages_with_table) + " is a page with tables on it.")
+# # START Get tables 1 page at a time.
+# more = 1
+# while more <= total_pages:
+#     print("Page " + str(more))
+#     pdf_tables.process_tables(chosen_pdf, int(more))
+#     more += 1
+# # END Get tables 1 page at a time.
+#
