02eb6a852ac42ed7b0e20c918cfa935568a841dc
AwesomeHawkk <outpatient@mac.com>
2018-11-27 13:02:10 -0800

    This adds functionality to clean up tables of garbage data.
    Former-commit-id: 1366f939a151b4b1ec8ca47ae596e75a494f77b7

--- external_modules/pdfScraper/driver.py
+++ external_modules/pdfScraper/driver.py
@@ -13,6 +13,9 @@ import pdf_tables
 import driver_methods
 import pdf_text
 import find_tables
+import pandas as pd
+pd.options.display.max_rows = 999
+pd.options.display.max_columns = 999
 
 
 # START This is getting the current pdfs in the pdf folder.
@@ -30,15 +33,15 @@ pdf_metadata.get_metadata(chosen_pdf)
 # START Get number of Pages
 total_pages = pdf_metadata.get_num_pages(chosen_pdf)
 # START Get number of Pages
-print("There are " + str(total_pages) + " pages in this document.")
+# print("There are " + str(total_pages) + " pages in this document.")
 
 
 # START get text from pdf
 text = pdf_text.convert_pdf_to_txt_looper(chosen_pdf, total_pages)
-print("The following is the entire text from the chosen pdf. \n")
+# print("The following is the entire text from the chosen pdf. \n")
 # The next line will give you guys the first page of the pdf. You can grab the whole array called "text"
 # and do what you want with it.
-print(text[0])
+# print(text[0])
 # End get text from pdf.
 
 # START Getting pages that have tables on them.
@@ -46,13 +49,15 @@ pages_with_table = find_tables.look_for_tables(text, total_pages)
 # END Getting pages that have tables on them.
 
 print(str(pages_with_table) + " is a page with tables on it.")
-# # START Get tables 1 page at a time.
-# more = 1
-# while more <= total_pages:
-#     print("Page " + str(more))
-#     pdf_tables.process_tables(chosen_pdf, int(more))
-#     more += 1
-# # END Get tables 1 page at a time.
-#
+
+# START Get tables 1 page at a time.
+more = len(pages_with_table)
+pwt_count = 0
+while pwt_count < more:
+    print("Page " + str(pages_with_table[pwt_count]))
+    pdf_tables.process_tables_get(chosen_pdf, int(pages_with_table[pwt_count]))
+    pwt_count += 1
+# END Get tables 1 page at a time.
+
 
 
--- external_modules/pdfScraper/pdf_tables.py
+++ external_modules/pdfScraper/pdf_tables.py
@@ -10,25 +10,64 @@ __date__ = "11/7/18"
 """
 
 from tabula import read_pdf
+import json
+import pandas as pd
+
+# pre_filter = df.to_json(orient='index')
+
+
+# START This function gets rid of unwanted cols and rows.
+def process_tables_clean(mdf):
+    row_count, col_count = mdf.shape
+    print(mdf.shape)
+    y = col_count -1
+    while y >= 0:
+        tally_col = x = 0
+        while x < row_count:
+            # print(str(mdf[y][x]))
+            if str(mdf.iloc[x][y]) == "REMOVE":
+                tally_col += 1
+            x += 1
+        if tally_col / row_count > .49:
+            if mdf.empty:
+                print('DataFrame is empty!')
+            else:
+                mdf = mdf.drop(mdf.columns[y], axis=1)
+                print(mdf)
+        y -= 1
+
+    if mdf.empty:
+        print('DataFrame is empty!')
+    else:
+        print(mdf)
+
+# END This function gets rid of unwanted cols and rows.
+
+
+# START This function marks fields that have bed data in them by putting teh work REMOVE in it's place.
+def process_tables_mark(df):
+    print(df)
+    row_count, col_count = df.shape
+    x = 0
+    while x < row_count:
+        y = 0
+        while y < col_count:
+            if len(str(df[y][x])) > 20:
+                df[y][x] = "REMOVE"
+            y += 1
+        x += 1
+    print(df)
+    return process_tables_clean(df)
+# END This function marks fields that have bed data in them by putting teh work REMOVE in it's place.
 
 
 # START This function processes a table import from a chosen pdf request.
-def process_tables(path, page):
+def process_tables_get(path, page):
     print(path + " is the name of the imported pdf.")
-    temp = read_pdf(path, output_format="dataframe", encoding="utf-8", java_options=None, pandas_options=None,
-                    multiple_tables=True, pages=int(page), silent=True)
-    print(temp)
+    dataframe_list = read_pdf(path, output_format="dataframe", encoding="utf-8",  multiple_tables=True, pages=int(page), silent=True)
+
+    if len(dataframe_list) > 0:
+        for x in dataframe_list:
+            process_tables_mark(x)
     return
 # END This function processes a table import request.
-
-
-# START This function allows the user to select a page that they want to get a table from.
-def select_page():
-    print("Please select the page you want to extract the table from:")
-    page_to_get = input()
-    return page_to_get
-# END This function allows the user to select a page that they want to get a table from.
-
-
-
-
