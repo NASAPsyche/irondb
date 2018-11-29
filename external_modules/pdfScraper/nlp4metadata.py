d2e7e0f2345fa0958ab3d5a6a0715f81fbb42e8d
Hajar <hboughou@asu.edu>
2018-11-28 22:15:38 -0700

    Optimizing title extraction: truncated to full

--- external_modules/pdfScraper/nlp4metadata.py
+++ external_modules/pdfScraper/nlp4metadata.py
@@ -1,12 +1,11 @@
 """
 nlp4metadata.py: Extracts metadata attributes from the text of a pdf
-				 Attributes include: title, authors, source
+Attributes include: title, authors, source
 """
 __authors__ = "Hajar Boughoula"
 __version__ = "2.0"
 __email__ = "hajar.boughoula@gmail.com"
 __date__ = "11/25/18"
-
 
 import pdf_text
 
@@ -14,29 +13,40 @@ import pdf_text
 path = 'C:/Users/Hajar/Desktop/Psyche NASA/irondb/external_modules/pdfScraper/pdfs/'
 page_num = 1
 
-# extracts the title from a user specified pdf
-def extract_title(pdf_name):
-	global page_num
-	randompage = pdf_text.convert_pdf_to_txt(path+pdf_name, page_num)
-	title = randompage.split('\n', 1)[0]
-	while (len(title.split()) <= 1) or (('Table' in title) == True):
-		page_num += 1
-		title = extract_title(pdf_name)
-	#print(page_num)
-	#print(randompage)
-	#or title[0].isdigit()
 
-	return title
+# extracts the title from a user specified pdf
+def truncated_title(pdf_name):
+    global page_num
+    random_page = pdf_text.convert_pdf_to_txt(path+pdf_name, page_num)
+
+    # extracts the truncated title from the top of a random page
+    title_trunc = random_page.split('\n', 1)[0]
+    while (len(title_trunc.split()) <= 1) or (('Table' in title_trunc) is True):
+        page_num += 1
+        title_trunc = truncated_title(pdf_name)
+
+    return title_trunc
+
+
+# finds truncated title in first page to return full title
+def extract_title(pdf_name):
+    first_page = pdf_text.convert_pdf_to_txt(path+pdf_name, 0)
+    relevant_data = (first_page.split("Abs", 1)[0])
+
+    title_split = truncated_title(pdf_name).split()
+    title_tagword = title_split[0] + ' ' + title_split[1]
+    title_index = (relevant_data.lower()).find(title_tagword.lower())
+    title_full = relevant_data[:title_index].rsplit('\n\n', 1)[1] + relevant_data[title_index:].split('\n', 1)[0]
+
+    return title_full
+
+
+# or title[0].isdigit()
 
 # WARNING: user input not supported in Sublime
 # user_pdf = input("Enter PDF name with extension: ")
 # print(extract_title(user_pdf))
 
 # in Sublime replace name of PDF here
-print(extract_title('Wassonetal_GCA_2007.pdf'))
-
-
-
-# split at Abstract to get text before that
-#with open('firstpage', 'r') as mypage:
-    #relevant_data = mypage.read().split("Abstract", 1)[0]
+print("Truncated Title:        " + truncated_title('WassonandChoe_GCA_2009.pdf'))
+print("Full Title:             " + extract_title('WassonandChoe_GCA_2009.pdf'))
