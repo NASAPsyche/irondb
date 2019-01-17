a7c00d198e0b0e39c24620ffeddab4c1bfe3040d
Troy Mullins <Troy.Mullins@asu.edu>
2019-01-17 00:20:33 -0500

    Task 450: Updated templates to display pdf after upload.

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,8 @@ gulp.task('js', function() {
   return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js',
     'node_modules/jquery/dist/jquery.min.js',
     'node_modules/tether/dist/js/tether.min.js',
-    'node_modules/popper.js/dist/popper.min.js'])
+    'node_modules/popper.js/dist/popper.min.js',
+    'node_modules/pdfobject/pdfobject.min.js'])
       .pipe(gulp.dest('public/javascripts'));
 });
 
