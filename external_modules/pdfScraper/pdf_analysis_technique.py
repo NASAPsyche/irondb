5519501e9e558c5fad9988322665100c4fed8245
Michael Falgien <mfalgien@gmail.com>
2019-01-19 14:58:44 -0800

    T470 & 469 to filter sentences out of paper that contain an analysis technique

--- external_modules/pdfScraper/pdf_analysis_technique.py
+++ external_modules/pdfScraper/pdf_analysis_technique.py
@@ -49,6 +49,7 @@ import find_tables
 # print(entire_pdf)
 
 
+
 def find_analysis_technique(pages, text):
     count = list()
     found = False
@@ -56,23 +57,23 @@ def find_analysis_technique(pages, text):
         if 'INAA' in text[i]:
             found = True
             print("INAA Found on page %i" % (i+1))
-            count.append(i+1)
+            count.insert(1,i+1)
         if 'RNAA' in text[i]:
             found = True
             print("RNAA Found on page %i" % (i + 1))
-            count.append(i+1)
+            count.insert(1,i+1)
         if 'LA-ICP-MS' in text[i]:
             found = True
             print("LA-ICP-MS Found on page %i" % (i + 1))
-            count.append(i+1)
+            count.insert(1,i+1)
         if 'neutron-activation analysis' in text[i]:
             found = True
             print("neutron activation analysis Found on page %i" % (i + 1))
-            count.append(i+1)
+            count.insert(1,i+1)
         if 'atomic-absorption' in text[i]:
             found = True
             print("atomic-absorption spectrophometry Found on page %i" % (i + 1))
-            count.append(i+1)
+            count.insert(1,i+1)
 
     if found == False:
         print("Analysis Technique Not Found")
--- external_modules/pdfScraper/pdf_sentence_tokenizer.py
+++ external_modules/pdfScraper/pdf_sentence_tokenizer.py
@@ -7,20 +7,20 @@ import pdf_metadata
 import pdf_text
 import re
 
+lexicon =   ['analysis','INAA', 'activation', 'neutron', 
+            'LA-ICP-MS', 'spectrophometry', 'radiochemical', 'RNAA']
 
 def sentence_tokenize(paper):
     # get page numbers
     total_pages = pdf_metadata.get_num_pages(paper)
 
-    entire_pdf = list()
     # get all text from pdf
+    entire_pdf = list()
     for i in range(total_pages):
         entire_pdf.append(pdf_text.convert_pdf_to_txt_looper(paper, i))
 
     # join list as string
-    entire_pdf_string = ' '.join(str(v) for v in entire_pdf)
-
-    string_text = ""
+    entire_pdf_string = join_text(entire_pdf)
 
     # filter text
     filtered_text = replace_char(entire_pdf_string)
@@ -30,12 +30,12 @@ def sentence_tokenize(paper):
     sent_dectector = nltk.data.load('tokenizers/punkt/english.pickle')
     processed_text = sent_dectector.tokenize(filtered_text)
 
-    for i in range(len(processed_text)):
-        string_text += processed_text[i] + '\n\n'
+    final_sent = find_analysis_fragments(processed_text)
 
-    # filter
+    # for i in range(len(processed_text)):
+    #   string_text += processed_text[i] + '\n\n'
     
-    return string_text
+    return final_sent
 
 def regexp_filter(text):
     string = ''
@@ -44,12 +44,44 @@ def regexp_filter(text):
         string += tokenizer.tokenize(words) + ' '
     return text
 
+# Replace new line characters in words
 def replace_char(text):
     string = ''
     for words in text.split():
        string += words.replace(r'\n', ' ') + ' '
     return string 
 
+# Join a list of text as a string
 def join_text(text):
     entire_string = ' '.join(str(v) for v in text)
     return entire_string
+
+def flatten_list(_list):
+    flat_list = list()
+    for sublist in _list:
+        if sublist != []:
+            flat_list.append(sublist)
+    return flat_list
+
+def find_analysis_fragments(paper):
+    analysis_list = list()
+    for sentence in paper:
+        #reset
+        for words in sentence.split():
+            for lex in lexicon:
+                if words.lower() == lex.lower():
+                    if sentence not in analysis_list:
+                        analysis_list.append(sentence)
+
+                       
+    if len(analysis_list) == 0:
+        analysis_list = ['No results found']
+    
+    analysis_string = convert_list_to_string(analysis_list)
+    return analysis_string
+
+def convert_list_to_string(_list):
+    final_string = ''
+    for i in range(len(_list)):
+        final_string += _list[i] + "\n\n"
+    return final_string
