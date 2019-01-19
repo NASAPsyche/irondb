bfc01492d440267e5db1ef95a9818956c2954f26
Michael Falgien <mfalgien@gmail.com>
2019-01-19 15:32:51 -0800

    linting and comments cuz i'm such a good boi

--- external_modules/pdfScraper/pdf_sentence_tokenizer.py
+++ external_modules/pdfScraper/pdf_sentence_tokenizer.py
@@ -1,3 +1,10 @@
+"""
+__authors__ = "Michael Falgien"
+__version__ = "1.0"
+__email__ = "mfalgien@gmail.com"
+__date__ = "1/19/2019"
+"""
+
 import nltk
 
 from nltk.tokenize import sent_tokenize, RegexpTokenizer
@@ -7,8 +14,12 @@ import pdf_metadata
 import pdf_text
 import re
 
-lexicon =   ['analysis','INAA', 'activation', 'neutron', 
-            'LA-ICP-MS', 'spectrophometry', 'radiochemical', 'RNAA']
+# list of known analysis techniques (not cap sensitive)
+lexicon = ['analysis', 'INAA', 'activation', 'neutron',
+           'LA-ICP-MS', 'spectrophometry', 'radiochemical', 'RNAA']
+
+# Main driver of finding the analysis techniques and returning text from pdfs
+
 
 def sentence_tokenize(paper):
     # get page numbers
@@ -25,17 +36,14 @@ def sentence_tokenize(paper):
     # filter text
     filtered_text = replace_char(entire_pdf_string)
 
-
     # Tokenize by sentence
     sent_dectector = nltk.data.load('tokenizers/punkt/english.pickle')
     processed_text = sent_dectector.tokenize(filtered_text)
 
     final_sent = find_analysis_fragments(processed_text)
 
-    # for i in range(len(processed_text)):
-    #   string_text += processed_text[i] + '\n\n'
-    
     return final_sent
+
 
 def regexp_filter(text):
     string = ''
@@ -45,16 +53,23 @@ def regexp_filter(text):
     return text
 
 # Replace new line characters in words
+
+
 def replace_char(text):
     string = ''
     for words in text.split():
-       string += words.replace(r'\n', ' ') + ' '
-    return string 
+        string += words.replace(r'\n', ' ') + ' '
+    return string
 
 # Join a list of text as a string
+
+
 def join_text(text):
     entire_string = ' '.join(str(v) for v in text)
     return entire_string
+
+# Flatten a list
+
 
 def flatten_list(_list):
     flat_list = list()
@@ -63,22 +78,27 @@ def flatten_list(_list):
             flat_list.append(sublist)
     return flat_list
 
+# compares words in sentences to words that exist in the lexicon
+# if a word is found, the entire sentence is added
+
+
 def find_analysis_fragments(paper):
     analysis_list = list()
     for sentence in paper:
-        #reset
         for words in sentence.split():
             for lex in lexicon:
                 if words.lower() == lex.lower():
                     if sentence not in analysis_list:
                         analysis_list.append(sentence)
 
-                       
     if len(analysis_list) == 0:
         analysis_list = ['No results found']
-    
+
     analysis_string = convert_list_to_string(analysis_list)
     return analysis_string
+
+# Converts a list to string for easier reading
+
 
 def convert_list_to_string(_list):
     final_string = ''
