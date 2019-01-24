608942519445a8fbc91f2011650047b5d6ba0c38
Troy Mullins <Troy.Mullins@asu.edu>
2019-01-24 10:45:52 -0500

    Task 487: Wrote conversion function for ppm to ppb.

--- __tests__/unit-conversion.test.js
+++ __tests__/unit-conversion.test.js
@@ -5,3 +5,9 @@ describe('Test percentToPPB function', () => {
     expect(convert.percentToPPB(7.5)).toBe(75000000);
   });
 });
+
+describe('Test ppmToPPB function', () => {
+  test('convert 750 ppm to 750000 ppb', () => {
+    expect(convert.ppmToPPB(750)).toBe(750000);
+  });
+});
--- controller/utils/unit-conversion.js
+++ controller/utils/unit-conversion.js
@@ -2,4 +2,7 @@ module.exports = {
   percentToPPB: function( num ) {
     return num * 10000000;
   },
+  ppmToPPB: function( num ) {
+    return num * 1000;
+  },
 };
