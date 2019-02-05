f83a393623c69ad744625117cf1129d1e89b1272
Troy Mullins <Troy.Mullins@asu.edu>
2019-02-05 12:23:55 -0500

    Task 632: Added placeholder routes.

--- controller/routes/panel.js
+++ controller/routes/panel.js
@@ -1,14 +1,24 @@
 const express = require('express');
 // eslint-disable-next-line new-cap
 const router = express.Router();
-const {isLoggedIn} = require('../middleware/auth');
+const {isAdmin, isLoggedIn} = require('../middleware/auth');
 
 router.get('/', isLoggedIn, function(req, res, next) {
   if (req.user.role === 'admin') {
-    // placeholder
+    res.redirect('/panel/admin');
   } else {
-    res.render('user-panel');
+    res.redirect('/panel/user');
   }
+});
+
+
+router.get('/admin', isAdmin, function(req, res, next) {
+  res.render('admin-panel');
+});
+
+
+router.get('/user', isLoggedIn, function(req, res, next) {
+  res.render('user-panel');
 });
 
 module.exports = router;
