373ad96d68397c479d531abc459eaf82cd38a145
AwesomeHawkk <outpatient@mac.com>
2019-02-05 19:04:54 -0800

    Cleaned up a lot and made my code great again. Getting tables is much faster than before. Also, I am now able to get multiple tables from one page now.

--- external_modules/pdfScraper/table_driver.py
+++ external_modules/pdfScraper/table_driver.py
@@ -7,8 +7,7 @@ __version__ = "1.0"
 __email__ = "jdjohn43@asu.edu"
 __date__ = "11/7/18"
 """
-import json
-import os
+
 import PyPDF2
 from tabula import read_pdf
 import pandas as pd
@@ -23,83 +22,33 @@ from pdfminer.pdfpage import PDFPage
 
 
 master_json = ""
+pages_with_tables = []
+master_dict_tables = {}
+pdf = ["pdfs/WassonandRichardson_GCA_2011.pdf",
+       "pdfs/WassonandChoe_GCA_2009.pdf",
+       "pdfs/Wasson_GCA_2017.pdf",
+       "pdfs/WassonandChoi_2003.pdf",
+       "pdfs/Litasov2018_Article_TraceElementCompositionAndClas.pdf",
+       "pdfs/Wasson_2010.pdf",
+       "pdfs/Wasson_2004.pdf",
+       "pdfs/Wassonetal_GCA_2007.pdf",
+       "pdfs/Ruzicka2014.pdf",
+       "pdfs/WassonandKallemeyn_GCA_2002.pdf",
+       "pdfs/RuzickaandHutson2010.pdf"]
+
+chosen_pdf = pdf[1]
 
 
-# START THIS IS TEMP DEV TEST CODE///////////////////////////////////////////////////////////////////////////////////////
-# /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-# START This function displays the names of the PDS in the folder called "pdfs".
-def display_pdf_names(directory):
-    num = 1
-    fileChoice = 0
-    while(int(fileChoice) < 1 or int(fileChoice) > num):
-        num = 0
-        fileNames = []
-        path = directory
-        dirs = os.listdir(path)
-
-        # This prints all the files and directories inside "pdfs"
-        for file in dirs:
-            if "pdf" in file:
-                print(str(num + 1) + ". " + file)
-                fileNames.append(file)
-                num += 1
-        print("Please choose the file you would like to import. \n")
-        fileChoice = input()
-        if(int(fileChoice) < 1 or int(fileChoice) > num):
-            print(str(fileChoice) + " Please choose a valid number. \n")
-
-    currentWorkingFile = fileNames[int(fileChoice) - 1]
-    return currentWorkingFile
-# END This function displays the names of the PDS in the folder called "pdfs".
-
-
-# START This function validates that input is an integer within a specific range.
-def input_number(message, hiNum, loNum):
-    while True:
-        try:
-            userInput = int(input(message))
-        except ValueError:
-            print("Not an integer! Try again.")
-            continue
-        else:
-            return userInput
-# START This function validates that input is an integer within a specific range.
-# END THIS IS TEMP DEV TEST CODE///////////////////////////////////////////////////////////////////////////////////////
-# /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-
-# START FUNCTIONS FUNCTIONS FUNCTIONS FUNCTIONS FUNCTIONS FUNCTIONS FUNCTIONS FUNCTIONS FUNCTIONS FUNCTIONS FUNCTIONS FUNCTIONS
-# START FUNCTIONS FUNCTIONS FUNCTIONS FUNCTIONS FUNCTIONS FUNCTIONS FUNCTIONS FUNCTIONS FUNCTIONS FUNCTIONS FUNCTIONS FUNCTIONS
-# START FUNCTIONS FUNCTIONS FUNCTIONS FUNCTIONS FUNCTIONS FUNCTIONS FUNCTIONS FUNCTIONS FUNCTIONS FUNCTIONS FUNCTIONS FUNCTIONS
-#
-#
-#
 # START 0. GETTING THE TEXT 0. GETTING THE TEXT 0. GETTING THE TEXT 0. GETTING THE TEXT 0. GETTING THE TEXT 0. GETTING THE TEXT
 # START 0. GETTING THE TEXT 0. GETTING THE TEXT 0. GETTING THE TEXT 0. GETTING THE TEXT 0. GETTING THE TEXT 0. GETTING THE TEXT
 
-# START Getting total pages of pdf
-def get_num_pages(path):
-    # created a pdf file object
-    pdfFileObj = open(path, 'rb')
-
-    # created a pdf reader object
-    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-
-    # This is the number of pages contained in the current pdf
-    numPagesPDF = pdfReader.numPages
-    return numPagesPDF
-# END Getting total pages of pdf
 
 # START This function imports raw text import from a chosen pdf request.
 def convert_pdf_to_txt(path, pageNo=0):
     text = ""
     rsrcmgr = PDFResourceManager()
     retstr = io.StringIO()
-    codec = 'utf-8'
-    laparams = LAParams()
-    device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+    device = TextConverter(rsrcmgr, retstr, codec='utf-8', laparams=LAParams())
     fp = open(path, 'rb')
     interpreter = PDFPageInterpreter(rsrcmgr, device)
 
@@ -114,13 +63,10 @@ def convert_pdf_to_txt(path, pageNo=0):
 # END This function imports raw text import from a chosen pdf request.
 
 # START This function puts each page of text in its own slot in an array of strings
-def convert_pdf_to_txt_looper(path, num_pages):
+def convert_pdf_to_txt_looper(path, total_pages):
     individual_pages = []
-    check = 0
-
-    while check <= num_pages - 1:
+    for check in range(total_pages):
         individual_pages.append(convert_pdf_to_txt(path, check))
-        check += 1
     return individual_pages
 # END This function puts each page of text in its own slot in an array of strings
 
@@ -128,79 +74,12 @@ def convert_pdf_to_txt_looper(path, num_pages):
 # END 0. GETTING THE TEXT 0. GETTING THE TEXT 0. GETTING THE TEXT 0. GETTING THE TEXT 0. GETTING THE TEXT 0. GETTING THE TEXT
 #
 #
-#
-# START 1. FINDING PAGES WITH TABLES FINDING PAGES WITH TABLES FINDING PAGES WITH TABLES FINDING PAGES WITH TABLES
-# START 1. FINDING PAGES WITH TABLES FINDING PAGES WITH TABLES FINDING PAGES WITH TABLES FINDING PAGES WITH TABLES
 
-# START This function finds pages in a pdf that have tables on it and makes an array of these pages in order to iterarte through
-# them later.
-def look_for_tables(pages, num_pages):
-    pages_with_tables = []
-    iterate = 0
-    while iterate <= num_pages - 1:
-        # print("test " + str(iterate))
-        splitted = pages[iterate].split()
-        if pages[iterate].find('\nTable ') > 0 or splitted[0] == "Table":
-            print("############# Table exists on Page " + str(iterate + 1) + " #############" + "\n Word: "
-            + str(pages[iterate].find('\nTable ')))
-            pages_with_tables.append(iterate + 1)
-        iterate += 1
-    return pages_with_tables
-# END This function finds pages in a pdf that have tables on it and makes an array of these pages in order to iterarte through
-# them later.
-
-
-# END 1. FINDING PAGES WITH TABLES FINDING PAGES WITH TABLES FINDING PAGES WITH TABLES FINDING PAGES WITH TABLES
-# END 1. FINDING PAGES WITH TABLES FINDING PAGES WITH TABLES FINDING PAGES WITH TABLES FINDING PAGES WITH TABLES
-#
-#
-#
-# START 2. GETTING TABLES GETTING TABLES GETTING TABLES GETTING TABLES GETTING TABLES GETTING TABLES GETTING TABLES GETTING TABLES
-# START 2. GETTING TABLES GETTING TABLES GETTING TABLES GETTING TABLES GETTING TABLES GETTING TABLES GETTING TABLES GETTING TABLES
-
-def process_tables_get(path, page):
-    dataframe_list = read_pdf(path, output_format="dataframe", encoding="utf-8",  multiple_tables=True, pages=int(page), silent=True)
-    print("This is the size of the DF: " + str(len(dataframe_list)))
-    if len(dataframe_list) > 0:
-        for i in dataframe_list[0].iterrows():
-            print("TEST" + str(i))
-
-    if len(dataframe_list) > 1:
-        print("This is 2nd DF: ")
-        for i in dataframe_list[1].iterrows():
-            print(i)
-    return dataframe_list
-
-
-# END 2. GETTING TABLES GETTING TABLES GETTING TABLES GETTING TABLES GETTING TABLES GETTING TABLES GETTING TABLES GETTING TABLES
-# END 2. GETTING TABLES GETTING TABLES GETTING TABLES GETTING TABLES GETTING TABLES GETTING TABLES GETTING TABLES GETTING TABLES
-#
-#
-#
-# START 3. MARKING THE TABLES MARKING THE TABLES MARKING THE TABLES MARKING THE TABLES MARKING THE TABLES MARKING THE TABLES
-# START 3. MARKING THE TABLES MARKING THE TABLES MARKING THE TABLES MARKING THE TABLES MARKING THE TABLES MARKING THE TABLES
-
-def process_tables_mark(df):
-    for x in range(df.shape[0]):
-        for y in range(df.shape[1]):
-            if len(str(df[y][x])) > 20:
-                df[y][x] = "REMOVE"
-        # print(df)
-    return df
-
-
-# END 3. MARKING THE TABLES MARKING THE TABLES MARKING THE TABLES MARKING THE TABLES MARKING THE TABLES MARKING THE TABLES
-# END 3. MARKING THE TABLES MARKING THE TABLES MARKING THE TABLES MARKING THE TABLES MARKING THE TABLES MARKING THE TABLES
-#
-#
-#
 # START 4. REMOVING BAD ROWS and COLS REMOVING BAD ROWS and COLS REMOVING BAD ROWS and COLS REMOVING BAD ROWS and COLS
 # START 4. REMOVING BAD ROWS and COLS REMOVING BAD ROWS and COLS REMOVING BAD ROWS and COLS REMOVING BAD ROWS and COLS
 
 def process_tables_clean(mdf):
     row_count, col_count = mdf.shape
-    # print(mdf.shape)
-    # print(mdf)
     col_iter = col_count - 1
     while col_iter >= 0:
         tally_col = row_iter = 0
@@ -220,106 +99,47 @@ def process_tables_clean(mdf):
 
 # END 4. REMOVING BAD ROWS and COLS REMOVING BAD ROWS and COLS REMOVING BAD ROWS and COLS REMOVING BAD ROWS and COLS
 # END 4. REMOVING BAD ROWS and COLS REMOVING BAD ROWS and COLS REMOVING BAD ROWS and COLS REMOVING BAD ROWS and COLS
-#
-#
-#
-# START 5. PUTTING TABLES INTO JSON PUTTING TABLES INTO JSON PUTTING TABLES INTO JSON PUTTING TABLES INTO JSON
-# START 5. PUTTING TABLES INTO JSON PUTTING TABLES INTO JSON PUTTING TABLES INTO JSON PUTTING TABLES INTO JSON
-
-def table_to_json(df_list):
-    table_json = {}
-    if len(df_list) > 0:
-        for x in df_list:
-            table_json = process_tables_mark(x).to_json(orient='index')
-    return table_json
-
-# END 5. PUTTING TABLES INTO JSON PUTTING TABLES INTO JSON PUTTING TABLES INTO JSON PUTTING TABLES INTO JSON
-# END 5. PUTTING TABLES INTO JSON PUTTING TABLES INTO JSON PUTTING TABLES INTO JSON PUTTING TABLES INTO JSON
-#
-#
-#
-# END FUNCTIONS FUNCTIONS FUNCTIONS FUNCTIONS FUNCTIONS FUNCTIONS FUNCTIONS FUNCTIONS FUNCTIONS FUNCTIONS FUNCTIONS FUNCTIONS
-# END FUNCTIONS FUNCTIONS FUNCTIONS FUNCTIONS FUNCTIONS FUNCTIONS FUNCTIONS FUNCTIONS FUNCTIONS FUNCTIONS FUNCTIONS FUNCTIONS
-# END FUNCTIONS FUNCTIONS FUNCTIONS FUNCTIONS FUNCTIONS FUNCTIONS FUNCTIONS FUNCTIONS FUNCTIONS FUNCTIONS FUNCTIONS FUNCTIONS
-
-
-def process_table_engine(path, page):
-    table_rec = process_tables_get(path, page)
-    print(table_rec)
-
-    if len(table_rec) > 0:
-        for x in table_rec:
-            table_marked = process_tables_mark(x)
-            print(table_marked)
-            # table_cleaned = process_tables_clean(table_marked)
-            # # print(table_cleaned)
-            # return json.loads((table_cleaned.to_json(double_precision=10, force_ascii=True,date_unit='ms', lines=False)))
-
-
-
-
-
-
-
-
-#
-# tableNum = 1
-# for x in array_tables:
-#     print("Table: " + str(tableNum))
-#     print( str(x))
-#     print("\n")
-#     tableNum += 1
-
-# START DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER
-# START DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER
-# START DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER
-
-
-# START This is getting the current pdfs in the pdf folder.
-chosen_pdf = display_pdf_names("pdfs/")
-print("\n You selected: " + chosen_pdf)
-# END This is getting the current pdfs in the pdf folder.
-
-chosen_pdf = "pdfs/" + chosen_pdf
-
 
 # START Get number of Pages
-total_pages = get_num_pages(chosen_pdf)
+total_pages = PyPDF2.PdfFileReader(open(pdf[1], 'rb')).numPages
 # START Get number of Pages
 # print("There are " + str(total_pages) + " pages in this document.")
 
-
 # START get text from pdf
 text = convert_pdf_to_txt_looper(chosen_pdf, total_pages)
-# print("The following is the entire text from the chosen pdf. \n")
-# print(text[0])
 # End get text from pdf.
 
 # START Getting pages that have tables on them.
-pages_with_table = look_for_tables(text, total_pages)
+for iterate in range(total_pages):
+    splitted = text[iterate].split()
+    if text[iterate].find('\nTable ') > 0 or splitted[0] == "Table":
+        print("############# Table exists on Page " + str(iterate + 1) + " #############" + "\n Word: "
+              + str(text[iterate].find('\nTable ')))
+        pages_with_tables.append(iterate + 1)
 # END Getting pages that have tables on them.
 
 # START Get tables 1 page at a time.
-more = len(pages_with_table)
-pwt_count = 0
-accepted_table = 1
-table_json_to_send = {}
-array_tables = []
+tables_rec_from_page = read_pdf(chosen_pdf, output_format="dataframe", encoding="utf-8",  multiple_tables=True, pages=pages_with_tables,
+                                    silent=True)
 
-while pwt_count < more:
-    test = process_table_engine(chosen_pdf, int(pages_with_table[pwt_count]))
-    if str(test) != "None":
-        table_built = {"table_" + str(accepted_table): test}
-        print("This is table: " + str(table_built))
-        array_tables.append(table_built)
-        accepted_table += 1
+print(len(tables_rec_from_page))
 
-    pwt_count += 1
-
-print(array_tables)
 # END Get tables 1 page at a time.
 
+# Start Marking the fields for removal
+if len(tables_rec_from_page) > 0:
+    for df in tables_rec_from_page:
+        for x in range(df.shape[0]):
+            for y in range(df.shape[1]):
+                if len(str(df[y][x])) > 20:
+                    df[y][x] = "REMOVE"
+print("START THE MARKING START THE MARKING START THE MARKING START THE MARKING START THE MARKING START THE MARKING ")
+print(tables_rec_from_page)
+# End Marking the fields for removal
 
-# END DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER
-# END DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER
-# END DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER DRIVER
+
+# table_cleaned = process_tables_clean(table_marked)
+# # print(table_cleaned)
+# return json.loads((table_cleaned.to_json(double_precision=10, force_ascii=True,date_unit='ms', lines=False)))
+
+
