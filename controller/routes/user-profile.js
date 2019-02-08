fed4145bb5491eda8c6810ba9cd6e78c46d39f89
Michael Falgien <mfalgien@gmail.com>
2019-02-07 19:24:11 -0800

    placeholder on server for query

--- controller/routes/user-profile.js
+++ controller/routes/user-profile.js
@@ -2,9 +2,23 @@ const express = require('express');
 // eslint-disable-next-line new-cap
 const router = express.Router();
 const {isLoggedIn} = require('../middleware/auth');
+const db = require('../db');
 
 router.get('/', isLoggedIn, function(req, res, next) {
   res.render('user-profile');
 });
 
+
+/* GET /panel/user  */
+router.get('/', isLoggedIn, function(req, res, next) {
+  db.query(
+      'SELECT * FROM pending_table WHERE status=$1',
+      ['active'],
+      (dbErr, dbRes) => {
+        if (dbErr) {
+          return next(dbErr);
+        }
+        res.render('user-profile', {Entries: dbRes.rows});
+      });
+});
 module.exports = router;
