c0559425e8e7e73b7585a30bdfdbfabd56f66a10
Michael Falgien <mfalgien@gmail.com>
2019-02-13 21:21:59 -0800

    changes to panel.js to render view and template to include a number for entries pending

--- controller/routes/panel.js
+++ controller/routes/panel.js
@@ -1,6 +1,7 @@
 const express = require('express');
 // eslint-disable-next-line new-cap
 const router = express.Router();
+const db = require('../db');
 const {isAdmin, isLoggedIn} = require('../middleware/auth');
 
 router.get('/', isLoggedIn, function(req, res, next) {
@@ -18,7 +19,18 @@ router.get('/admin', isAdmin, function(req, res, next) {
 
 
 router.get('/user', isLoggedIn, function(req, res, next) {
-  res.render('user-panel');
+  db.query(
+      /* eslint-disable-next-line max-len */
+      'SELECT nomenclature, title, published_year, author_name FROM full_attributions_pending', [],
+      (dbErr, dbRes) => {
+        if (dbErr) {
+          return next(dbErr);
+        }
+        console.log(dbRes);
+        res.render('user-panel', {
+          Entries: dbRes.rows,
+          Approval: dbRes.rowCount,
+        });
+      });
 });
-
 module.exports = router;
