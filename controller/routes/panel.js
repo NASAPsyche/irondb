a4aa27fd8ef66da52b685865ddfca09f09e864f7
Michael Falgien <mfalgien@gmail.com>
2019-02-15 18:11:16 -0800

    changes to server to query both tables and updated template for reported entries

--- controller/routes/panel.js
+++ controller/routes/panel.js
@@ -20,17 +20,24 @@ router.get('/admin', isAdmin, function(req, res, next) {
 
 router.get('/user', isLoggedIn, function(req, res, next) {
   db.query(
-      /* eslint-disable-next-line max-len */
-      'SELECT nomenclature, title, published_year, author_name FROM full_attributions_pending', [],
-      (dbErr, dbRes) => {
-        if (dbErr) {
-          return next(dbErr);
+      'SELECT * FROM full_attributions_pending', [],
+      (dbErr1, dbRes1) => {
+        if (dbErr1) {
+          return next(dbErr1);
         }
-        console.log(dbRes);
-        res.render('user-panel', {
-          Entries: dbRes.rows,
-          Approval: dbRes.rowCount,
-        });
+        db.query(
+        /* eslint-disable-next-line max-len */
+            'SELECT * FROM full_attributions_flagged', [], (dbErr2, dbRes2) => {
+              if (dbErr2) {
+                return next(dbErr2);
+              }
+              res.render('user-panel', {
+                Pending: dbRes1.rows,
+                pendingCount: dbRes1.rowCount,
+                Flagged: dbRes2.rows,
+                flaggedCount: dbRes2.rowCount,
+              });
+            });
       });
 });
 module.exports = router;
