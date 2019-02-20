e61d70455692a700fba43d8e21c2f0e06809c0e8
Troy Mullins <Troy.Mullins@asu.edu>
2019-02-20 16:33:13 -0500

    Task 740: Updated js for post request.

--- public/javascripts/db-export.js
+++ public/javascripts/db-export.js
@@ -8,13 +8,52 @@ $( 'i' ).click(function() {
   }
 });
 
-// On submit of the form
-// collect entry ids from hidden elements and submit post request.
-$( 'form' ).on('click', function() {
-  $( '#entry-ids' ).empty();
-  $( 'th:hidden' ).each(function() {
-    // eslint-disable-next-line max-len
-    $( '<input>' ).attr({type: 'hidden', name: 'entries'}).val( $(this).text() ).appendTo( $( '#entry-ids' ) );
-  });
-});
 
+$( '#export-form' ).submit( function( event ) {
+  // Get fields
+  const fields = $( 'thead > tr > th' ).map(function() {
+    if (this.innerText !== '') {
+      return this.innerText;
+    }
+  }).get();
+
+  // Get data
+  const data = [];
+  let currentRow = {};
+  $('tbody > tr').map(function() {
+    $.each( this.children, function(index, value) {
+      console.log(value);
+      if (index === 0) {
+        data.push(currentRow);
+        currentRow = {};
+      } else {
+        // console.log('-----------------------------------------');
+        // console.log('current field: ' + fields[index - 1]);
+        // console.log('current value: ' + value);
+        // console.log('current textContent: ' + value.textContent);
+        // console.log('null test ' + value.textContent === null);
+        // console.log('current innerText: ' + value.innerText);
+        // console.log('type of inner' + typeof value.innerText);
+        // console.log('-----------------------------------------');
+
+        currentRow[fields[index - 1]] = (value.textContent.search(/\n/) !== -1)
+            ? value.innerText : value.textContent;
+      }
+
+      if (data.length === index) {
+        data.shift();
+        data.push(currentRow);
+      }
+    });
+  });
+
+  // Combine for post request
+  const postData = {
+    'fields': fields,
+    'data': data,
+  };
+
+  $('#tableData').val(JSON.stringify(postData));
+
+  return true;
+});
