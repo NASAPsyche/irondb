f5980f60e7a79bc9ee2aa1aab9b3fb9cdd586d5d
Troy Mullins <Troy.Mullins@asu.edu>
2019-02-21 06:18:17 -0500

    Task 747: Fixed last row bug.

--- public/javascripts/db-export.js
+++ public/javascripts/db-export.js
@@ -20,14 +20,21 @@ $( '#export-form' ).submit( function( event ) {
   // Get data
   const data = [];
   let currentRow = {};
+  let isFirstRow = true;
+  const numRows = $('tbody > tr').length;
   $('tbody > tr').map(function() {
     $.each( this.children, function(index, value) {
-      if (index === 0) {
+      if (index === 0 && !isFirstRow) {
         data.push(currentRow);
         currentRow = {};
       } else {
         currentRow[fields[index - 1]] = (value.textContent.search(/\n/) !== -1)
             ? value.innerText : value.textContent;
+      }
+      isFirstRow = false;
+
+      if (index === fields.length && data.length === (numRows - 1)) {
+        data.push(currentRow);
       }
     });
   });
