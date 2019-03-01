df766348285eb4f80f76a11f407286a96c5b30ca
Troy Mullins <Troy.Mullins@asu.edu>
2019-02-28 19:14:35 -0500

    Task 759: Updated get route.

--- controller/routes/database/meteorite.js
+++ controller/routes/database/meteorite.js
@@ -5,15 +5,55 @@ const router = new Router();
 
 /* GET single entry */
 router.get('/:id', async (req, res, next) => {
-  let isSignedIn = false;
-  if (req.isAuthenticated()) isSignedIn = true;
+  let paperID = -1;
+  if (req.query.hasOwnProperty('paper')) {
+    paperID = parseInt(req.query.paper, 10);
+  }
+
+  let resObj = [];
   try {
-    const queryString = 'SELECT * FROM complete_table WHERE entry_id=$1';
-    const argsArray = [req.params.id];
-    const {rows} = await db.aQuery(queryString, argsArray);
-    res.render('single-entry', {Entries: rows, isSignedIn: isSignedIn});
+    const Entries = db.aQuery(
+        'SELECT * FROM complete_table WHERE entry_id=$1 LIMIT 1',
+        [req.params.id]
+    );
+    const PaperList = db.aQuery(
+        'SELECT DISTINCT paper_id, title FROM complete_table WHERE entry_id=$1',
+        [req.params.id]
+    );
+    let queryString = 'SELECT * FROM elements_with_bodies_groups_active';
+    queryString += ' WHERE body_id=$1 AND paper_id=$2 AND ppb_mean > 10000000';
+    const MajorElements = db.aQuery(
+        queryString, [req.params.id, paperID]
+    );
+    queryString = 'SELECT * FROM elements_with_bodies_groups_active';
+    queryString += ' WHERE body_id=$1 AND paper_id=$2 AND';
+    queryString += ' ppb_mean <= 10000000 AND ppb_mean >= 1000000';
+    const MinorElements = db.aQuery(
+        queryString, [req.params.id, paperID]
+    );
+    queryString = 'SELECT * FROM elements_with_bodies_groups_active';
+    queryString += ' WHERE body_id=$1 AND paper_id=$2 AND ppb_mean < 1000000';
+    const TraceElements = db.aQuery(
+        queryString, [req.params.id, paperID]
+    );
+
+    resObj = await Promise.all([
+      Entries, PaperList,
+      MajorElements, MinorElements,
+      TraceElements,
+    ]);
   } catch (err) {
     next(createError(500));
+  } finally {
+    res.render('single-body', {
+      isSignedIn: req.isAuthenticated(),
+      PaperID: paperID,
+      Entries: resObj[0].rows,
+      PaperList: resObj[1].rows,
+      MajorElements: resObj[2].rows,
+      MinorElements: resObj[3].rows,
+      TraceElements: resObj[4].rows,
+    });
   }
 });
 
