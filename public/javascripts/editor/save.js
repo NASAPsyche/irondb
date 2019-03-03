8f6d0bef09a7ef88528b9bd7fff08587bbc5f9ed
Ken Bonilla <kenbonilla@gmail.com>
2019-03-03 16:46:24 -0600

    use jquery to post json to save-route

--- public/javascripts/editor/save.js
+++ public/javascripts/editor/save.js
@@ -6,28 +6,53 @@
 $(document).ready(function() {
   $('#pdf-form').submit(function(event) {
     event.preventDefault(); // do not submit
-    const serializedData = $('#insert-form').serializeArray();
-    const jsondata_ = {};
-    serializedData.forEach(function(element) {
-      jsondata_[element['name']] = element['value'];
+    const jsonString = serializeForm();
+    $.ajax({
+      url: '/data-entry/save',
+      type: 'POST',
+      data: jsonString,
+      contentType: 'application/json; charset=utf-8',
+      dataType: 'json',
+      success: function(data, status, jqXHR) {
+        alert(status);
+      },
+
+      error: function(jqXHR, status) {
+        // error handler
+        console.log(jqXHR);
+        alert('fail' + status.code);
+      },
     });
-    // eslint-disable-next-line no-undef
-    const username_ = username; // username defined in ejs from route
-    // eslint-disable-next-line no-undef
-    const filename_ = filename; // filename defined in ejs from route
 
-    const jsonsend = {
-      username: username_,
-      data: jsondata_,
-      pdf_path: filename_,
-    };
 
-    // Send a post request to save the data
-    const xhr = new XMLHttpRequest();
-    xhr.open('POST', '/data-entry/save', true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(JSON.stringify(jsonsend));
     // eslint-disable-next-line no-invalid-this
-    this.submit(); // ok, submit
+    // this.submit(); // ok, submit
   });
 });
+
+/**
+ * @description serialize the insert form
+ * @return {string} returns string with username, form data, and pdf string
+ */
+function serializeForm() {
+  const serializedData = $('#insert-form').serializeArray();
+  console.log(serializedData);
+  const jsondata_ = {};
+  serializedData.forEach(function(element) {
+    jsondata_[element['name']] = element['value'];
+  });
+  console.log(jsondata_);
+  // eslint-disable-next-line no-undef
+  const username_ = username; // username defined in ejs from route
+  // eslint-disable-next-line no-undef
+  const filename_ = filename; // filename defined in ejs from route
+
+  const fullJson = {
+    username: username_,
+    data: jsondata_,
+    pdf_path: filename_,
+  };
+
+  console.log(fullJson);
+  return (JSON.stringify(fullJson));
+}
