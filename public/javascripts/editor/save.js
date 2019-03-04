3e960abb054d2f0b7e1ad6417e83a6bf83acefbb
Ken Bonilla <kenbonilla@gmail.com>
2019-03-04 13:36:49 -0600

    remove unused code

--- public/javascripts/editor/save.js
+++ public/javascripts/editor/save.js
@@ -82,30 +82,3 @@ function postSave(jsonString) {
   });
   return flag;
 }
-
-// $(document).ready(function() {
-//   $('#save-btn').click(function() {
-//     const serializedData = $('#insert-form').serializeArray();
-//     const jsondata_ = {};
-//     serializedData.forEach(function(element) {
-//       jsondata_[element['name']] = element['value'];
-//     });
-//     // eslint-disable-next-line no-undef
-//     const username_ = username; // username defined in ejs from route
-//     // eslint-disable-next-line no-undef
-//     const filename_ = filename; // filename defined in ejs from route
-
-//     const jsonsend = {
-//       username: username_,
-//       data: jsondata_,
-//       pdf_path: filename_,
-//     };
-
-//     // Send a post request to save the data
-//     const xhr = new XMLHttpRequest();
-//     xhr.open('POST', '/data-entry/save', true);
-//     xhr.setRequestHeader('Content-Type', 'application/json');
-//     xhr.send(JSON.stringify(jsonsend));
-//     alert('Saved the form');
-//   });
-// });
