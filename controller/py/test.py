570f6914fb26b0d241a5628955533ea91543af84
AwesomeHawkk <outpatient@mac.com>
2019-03-14 12:39:14 -0700

    This is a simple file to test PythonShell with.

---
+++ controller/py/test.py
@@ -0,0 +1,11 @@
+import PyPDF2
+import sys
+import os
+import json
+from tabula import read_pdf
+import pandas as pd
+pd.options.display.max_rows = 999
+pd.options.display.max_columns = 999
+
+# print(sys.argv[0])
+print("This is a text")
