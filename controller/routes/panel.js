671e619d932c9180627cb401be74c868dc64a94b
Michael Falgien <mfalgien@gmail.com>
2019-03-16 16:53:35 -0700

    T930 adding pending approval and reported entries views

--- controller/routes/panel.js
+++ controller/routes/panel.js
@@ -14,7 +14,26 @@ router.get('/', isLoggedIn, function(req, res, next) {
 
 
 router.get('/admin', isAdmin, function(req, res, next) {
-  res.render('admin-panel');
+  db.query(
+      'SELECT * FROM full_attributions_pending', [],
+      (dbErr1, dbRes1) => {
+        if (dbErr1) {
+          return next(dbErr1);
+        }
+        db.query(
+        /* eslint-disable-next-line max-len */
+            'SELECT * FROM full_attributions_flagged', [], (dbErr2, dbRes2) => {
+              if (dbErr2) {
+                return next(dbErr2);
+              }
+              res.render('admin-panel', {
+                Pending: dbRes1.rows,
+                pendingCount: dbRes1.rowCount,
+                Flagged: dbRes2.rows,
+                flaggedCount: dbRes2.rowCount,
+              });
+            });
+      });
 });
 
 
