ac1b5760c460ead8b97e47665b77be3a89baeca1
Michael Falgien <mfalgien@gmail.com>
2019-03-18 18:45:44 -0700

    T931 updating template for manage user table and updating the server with queries

--- controller/routes/panel.js
+++ controller/routes/panel.js
@@ -26,11 +26,19 @@ router.get('/admin', isAdmin, function(req, res, next) {
               if (dbErr2) {
                 return next(dbErr2);
               }
-              res.render('admin-panel', {
-                Pending: dbRes1.rows,
-                pendingCount: dbRes1.rowCount,
-                Flagged: dbRes2.rows,
-                flaggedCount: dbRes2.rowCount,
+              /* eslint-disable-next-line max-len */
+              db.query('SELECT t1.user_id, t1.username, t1.role_of FROM users as t1', [], (dbErr3, dbRes3) => {
+                if (dbErr3) {
+                  return next(dbErr3);
+                }
+                res.render('admin-panel', {
+                  Pending: dbRes1.rows,
+                  pendingCount: dbRes1.rowCount,
+                  Flagged: dbRes2.rows,
+                  flaggedCount: dbRes2.rowCount,
+                  Users: dbRes3.rows,
+                  userCount: dbRes3.rowCount,
+                });
               });
             });
       });
