055243fc62597a7603d151c0d163d040c2924847
Troy Mullins <Troy.Mullins@asu.edu>
2019-03-19 05:02:19 -0400

    Task 951: Fixed 413 error bug.

--- controller/app.js
+++ controller/app.js
@@ -97,7 +97,8 @@ app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
 
 // Middleware
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
