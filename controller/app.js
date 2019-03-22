0bef2723ebce068e4167dc9b7d492f8a99ffb3ce
Michael Falgien <mfalgien@gmail.com>
2019-03-22 16:08:58 -0700

    use statusCode not status for 500

--- controller/app.js
+++ controller/app.js
@@ -166,7 +166,7 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   // eslint-disable-next-line max-len
-  if (res.status === 500) {
+  if (res.statusCode == 500) {
     // eslint-disable-next-line max-len
     res.render('error', {isSignedIn: isSignedIn, message: 'Internal Server Error', errcode: 'Error 500'});
   } else {
