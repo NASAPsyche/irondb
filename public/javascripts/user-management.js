ff8a42b380ac3fe332a75888c20cfa41d3202ff1
Michael Falgien <mfalgien@gmail.com>
2019-03-30 12:53:50 -0700

    jquery to determine which roles where changed

--- public/javascripts/user-management.js
+++ public/javascripts/user-management.js
@@ -17,8 +17,60 @@ $(document).ready(function() {
  */
 $(document).ready(function() {
   $('#confirm').on('click', function() {
-    alert('button clicked. Nothing happens yet :(');
+    let str = 'Making the following changes: \n';
+    for (let i = 0; i < data.length; i++) {
+      // eslint-disable-next-line max-len
+      str += `user ${data[i].user} from ${data[i].current} to ${data[i].role} \n`;
+    }
+    alert(str);
+    postData(data);
   });
 });
 
+
+const data = [];
+
+/**
+ * @description get the ID of the user and new role for any changed dropdowns.
+ * Display alert if new role is not equal to previous role and add to data that
+ * will be sent to server.
+ */
+$(document).ready(function() {
+  $('td').change(function() {
+    const userID = $(this).closest('tr').find('td:first').text().trim();
+    // eslint-disable-next-line max-len
+    const newRole = $(this).closest('tr').find('#user-role :selected').text().trim();
+    // eslint-disable-next-line max-len
+    const currentRole = $(this).closest('tr').find('#current-role').text().trim();
+    console.log(userID);
+    console.log(newRole);
+
+    if (currentRole != newRole) {
+      alert(userID + '\'s role changed from ' + currentRole + ' to ' + newRole);
+      data.push({'user': userID, 'current': currentRole, 'role': newRole});
+    }
+  });
+});
+
+/**
+ * Send stuff
+ * @param {*} jsonString
+ */
+async function postData(jsonString) {
+  await $.ajax({
+    url: '/users/update',
+    type: 'POST',
+    data: JSON.stringify(jsonString),
+    contentType: 'application/json; charset=utf-8',
+    dataType: 'json',
+    async: true,
+    success: function(data, status, jqXHR) {
+      alert(status);
+      return true;
+    },
+    error: function(jqXHR, status) {
+      return false;
+    },
+  });
+}
 
