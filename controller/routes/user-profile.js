907c70e9b7735007e44c8c854405d2380bbf3d88
Michael Falgien <mfalgien@gmail.com>
2019-04-04 14:36:56 -0700

    T764 post route and db transactions

--- controller/routes/user-profile.js
+++ controller/routes/user-profile.js
@@ -14,12 +14,45 @@ router.get('/', isLoggedIn, async (req, res, next) => {
     // eslint-disable-next-line max-len
     const user = db.aQuery(`SELECT * FROM users_with_info WHERE user_id = ${userID}`, []);
     resObj = await Promise.all([user]);
+    console.log(`AFTER IS ${JSON.stringify(resObj[0].rows)}`);
   } catch (err) {
     next(createError(500));
   } finally {
     res.render('profile', {User: resObj[0].rows});
-    console.log(`USER IS ${JSON.stringify(resObj[0].rows)}`);
   }
+});
+
+router.post('/update', isLoggedIn, async (req, res, next) => {
+  const client = await db.pool.connect();
+
+  // queries
+  // eslint-disable-next-line max-len
+  const updateFirstName = `UPDATE user_info SET first_name = $1 WHERE  user_id = $2`;
+  const insertFirstName = [req.body.first_name, req.body.user_id];
+  // eslint-disable-next-line max-len
+  const updateLastName = `UPDATE user_info SET last_name = $1 WHERE  user_id = $2`;
+  const insertLastName = [req.body.last_name, req.body.user_id];
+  // eslint-disable-next-line max-len
+  const updateEmail = `UPDATE user_info SET email_address = $1 WHERE  user_id = $2`;
+  const insertEmail = [req.body.email, req.body.user_id];
+
+  try {
+    await client.query('BEGIN');
+    await client.query(updateFirstName, insertFirstName);
+    await client.query('COMMIT');
+    await client.query('BEGIN');
+    await client.query(updateLastName, insertLastName);
+    await client.query('COMMIT');
+    await client.query('BEGIN');
+    await client.query(updateEmail, insertEmail);
+    await client.query('COMMIT');
+  } catch (e) {
+    await client.query('ROLLBACK');
+    next(createError(500));
+  } finally {
+    client.release();
+  }
+  res.json({ok: true});
 });
 
 module.exports = router;
--- public/javascripts/profile.js
+++ public/javascripts/profile.js
@@ -11,13 +11,67 @@ $(document).ready(function() {
 });
 
 $(document).ready(function() {
-  $('#updatepassword').change(function() {
-    if ($('#updatepassword').prop('checked', true)) {
+  $('#updatepassword').on('change', function() {
+    if ($('#updatepassword').is(':checked')) {
       $('#confirm-password').removeAttr('hidden');
       $('#confirm-password').removeAttr('readonly');
       $('#confirmLabel').removeAttr('hidden');
       $('#password').removeAttr('readonly');
       $('#password').attr('placeholder', '');
+      $('#password').removeAttr('hidden');
+      $('#passwordLabel').removeAttr('hidden');
+    } else {
+      $('#confirm-password').attr('hidden', true);
+      $('#confirmLabel').attr('hidden', true);
+      $('#password').attr('hidden', true);
+      $('#passwordLabel').attr('hidden', true);
     }
   });
 });
+
+const data = {
+  'first_name': '',
+  'last_name': '',
+  'username': '',
+  'user_id': '',
+  'email': '',
+  'password': '',
+};
+
+$(document).ready(async function() {
+  $('#user-update-form').submit(async function(event) {
+    data.first_name = $('#firstname').val();
+    data.last_name = $('#lastname').val();
+    data.username = $('#username').val();
+    data.email = $('#email').val();
+    data.password = $('#password').val();
+    data.user_id = parseInt($('#userID').val());
+    alert(`sending the following ${JSON.stringify(data)}`);
+
+    const jsonData = JSON.stringify(data);
+    await postData(jsonData);
+    window.location.reload();
+  });
+});
+
+/**
+ * Send data to server
+ * @param {*} jsonString
+ */
+async function postData(jsonString) {
+  await $.ajax({
+    url: '/profile/update',
+    type: 'POST',
+    data: jsonString,
+    contentType: 'application/json; charset=utf-8',
+    dataType: 'json',
+    async: true,
+    success: function(data, status, jqXHR) {
+      console.log();
+      return true;
+    },
+    error: function(jqXHR, status) {
+      return false;
+    },
+  });
+}
