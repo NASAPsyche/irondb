bccbccc5fc3ce5aa321618f1eab35675030baef6
Michael Falgien <mfalgien@gmail.com>
2019-04-03 18:56:58 -0700

    T761 initial view and get route with server information to show in fields

--- controller/routes/user-profile.js
+++ controller/routes/user-profile.js
@@ -1,25 +1,26 @@
-const express = require('express');
+const Router = require('express-promise-router');
 // eslint-disable-next-line new-cap
-const router = express.Router();
+const router = new Router();
+const createError = require('http-errors');
 const {isLoggedIn} = require('../middleware/auth');
 const db = require('../db');
 
-// router.get('/', isLoggedIn, function(req, res, next) {
-//  res.render('user-profile');
-// });
 
-
-/* GET /panel/user  */
-router.get('/', isLoggedIn, function(req, res, next) {
-  db.query(
-      /* eslint-disable-next-line max-len */
-      'SELECT nomenclature, title, published_year, author_name FROM full_attributions_pending', [],
-      (dbErr, dbRes) => {
-        if (dbErr) {
-          return next(dbErr);
-        }
-        console.log(dbRes);
-        res.render('user-panel', {Entries: dbRes.rows});
-      });
+/* GET /profile  */
+router.get('/', isLoggedIn, async (req, res, next) => {
+  const userID = req.user.id;
+  let resObj = [];
+  try {
+    // eslint-disable-next-line max-len
+    const user = db.aQuery(`SELECT * FROM users_with_info WHERE user_id = ${userID}`, []);
+    resObj = await Promise.all([user]);
+  } catch (err) {
+    next(createError(500));
+  } finally {
+    res.render('profile', {User: resObj[0].rows});
+    console.log(`USER IS ${JSON.stringify(resObj[0].rows)}`);
+  }
 });
+
 module.exports = router;
+
