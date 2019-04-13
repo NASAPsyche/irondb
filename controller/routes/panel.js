ea0ff4017468b1ea8694cfa69d156d6066696850
Troy Mullins <Troy.Mullins@asu.edu>
2019-04-13 01:18:43 -0400

    Task 1198: Updated user panel to show own entries.

--- controller/routes/panel.js
+++ controller/routes/panel.js
@@ -46,7 +46,11 @@ router.get('/user', isLoggedIn, async (req, res, next) => {
     const Pending = db.aQuery('SELECT * FROM pending_entries_panel', []);
     // Flagged Unimplemented due to timeline limitation
     const Flagged = db.aQuery('SELECT * FROM full_attributions_flagged', []);
-    resObj = await Promise.all([Pending, Flagged]);
+    const OwnEntries = db.aQuery(
+        'SELECT * FROM all_papers_with_authors WHERE submitted_by = $1',
+        [req.user.username]
+    );
+    resObj = await Promise.all([Pending, Flagged, OwnEntries]);
   } catch (err) {
     next(createError(500));
   } finally {
@@ -55,6 +59,8 @@ router.get('/user', isLoggedIn, async (req, res, next) => {
       pendingCount: resObj[0].rowCount,
       Flagged: resObj[1].rows,
       flaggedCount: resObj[1].rowCount,
+      OwnEntries: resObj[2].rows,
+      ownCount: resObj[2].rowCount,
     });
   }
 });
