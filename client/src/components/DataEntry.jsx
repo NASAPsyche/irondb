c07b5db75cdb1f4eed6ceb47a97ab2e2de214cf8
Jonathan Rasmussen <jrasmus4@asu.edu>
2019-11-14 23:57:37 -0700

    US-114 T-149 Create shell components for routes

---
+++ client/src/components/DataEntry.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function DataEntry() {
+    return (
+        <div className="DataEntry">
+            <h2>Data Entry View</h2>
+        </div>
+    );
+}
+
+export default DataEntry;
