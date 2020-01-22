99d5587f445848b90804f70817a48d1e9a4ba859
mjidris <m4idris@gmail.com>
2020-01-21 22:34:59 -0800

    US173: Add top margin to error message

--- client/src/components/Error.jsx
+++ client/src/components/Error.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Error({ error }) {
     return (
-        <div class="container-fluid">            
+        <div class="container-fluid  err-msg">
             <h1 class="text-danger">Error: {error}</h1>
         </div>
     )
