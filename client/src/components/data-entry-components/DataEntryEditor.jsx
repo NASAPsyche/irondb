7a601c6b2fd92a69e7ea7156200eec3c3b663592
Jonathan Rasmussen <jrasmus4@asu.edu>
2020-02-08 22:56:22 -0700

    US-163 T-228 Complete manual data entry scaffolding

--- client/src/components/data-entry-components/DataEntryEditor.jsx
+++ client/src/components/data-entry-components/DataEntryEditor.jsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useState, useEffects } from 'react';
+import DataEntryForm from './DataEntryForm'
 
-const DataEntryEditor = () => (
-    <h1>Data Entry Editor</h1>
-)
+const DataEntryEditor = () => {
+    const [meteoriteData, setData] = useState({
+        data: null,
+        elements: [],
+        techniques: [],
+    })
+
+    return (
+        <div class="container-fluid pt-1 pb-4" id="event-div">
+            <div class="d-flex flex-row align-items-center justify-content-center">
+                <div class="card text-center border-dark">
+                    <div class="card-header bg-danger">
+                        <h3 class="card-title text-warning">Editor</h3>
+                    </div>
+
+                    <div class="card-body tab-content" id="myTabContent">
+                        <div class="tab-pane fade show active" id="insert" role="tabpanel" aria-labelledby="insert-tab">
+
+                            <form method="POST" action="/data-entry/insert" class="align-top mx-auto text-left" id="insert-form">
+                                <DataEntryForm elements={meteoriteData.elements} techniques={meteoriteData.techniques} />
+                            </form>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
 
 export default DataEntryEditor;
