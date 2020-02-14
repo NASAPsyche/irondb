d4cc55b3ce0d4787d747f4cdadf948da34a1a19a
Cody Brobston <cbrobston22@gmail.com>
2020-02-14 15:27:28 -0800

    Initial commit for Account page.

---
+++ client/src/components/pages/Account.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Account = props => {
+
+    return (
+        <div className="accountPage">
+            <h1>Account Info:</h1>
+        </div>
+    );
+
+}
+export default Account;
