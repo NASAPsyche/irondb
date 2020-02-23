798b5a6b5576de7d86055abf1616cb7bb98e8b13
Cody Brobston <cbrobston22@gmail.com>
2020-02-22 21:27:25 -0800

    Added routing to the new Account page.

--- client/src/components/pages/Account.jsx
+++ client/src/components/pages/Account.jsx
@@ -4,10 +4,10 @@ const Account = () => {
 
     return (
         <div className="container-fluid pt-3 pb-5" id="top-container">
-                <div className="d-flex flex-row align-items-center justify-content-center mt-5 mb-2">
-                    <h1>Account page goes here</h1>
-                </div>
+            <div className="d-flex flex-row align-items-center justify-content-center mt-5 mb-2">
+                <h1>Account page goes here</h1>
             </div>
+        </div>
     );
 
 }
