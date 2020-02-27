a258bc9d2cb4bb647fd87171d71b7ef6a9f53889
Cody Brobston <cbrobston22@gmail.com>
2020-02-26 21:43:21 -0800

    Added basic form with static values.

--- client/src/components/pages/Account.jsx
+++ client/src/components/pages/Account.jsx
@@ -3,9 +3,44 @@ import React from 'react';
 const Account = () => {
 
     return (
-        <div className="container-fluid pt-3 pb-5" id="top-container">
-            <div className="d-flex flex-row align-items-center justify-content-center mt-5 mb-2">
-                <h1>Account page goes here</h1>
+        <div class='container-fluid mt-5 col-8 pb-4'>
+            <div class='card mt-5'>
+                <div class="container-fluid">
+                    <div class="pt-3 h1">
+                        <label>User:</label>
+                    </div>
+
+                    {/* TODO: add action and method */}
+                    <form id="user-update-form">
+                        <div class="form-group">
+                            <label>Username</label>
+                            <input type="text" class="form-control" id="username" value="User" readonly />
+                        </div>
+                        <div class="form-row form-group">
+                            <div class="col">
+                                <label>First Name</label>
+                                <input type="text" class="form-control" id="firstname" value="First" readonly required />
+                            </div>
+                            <div class="col">
+                                <label>Last Name</label>
+                                <input type="text" class="form-control" id="lastname" value="Last" readonly required />
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label>Email address</label>
+                            <input type="email" class="form-control" id="email" value="email@email.com" readonly />
+                        </div>
+                        <div class="form-group">
+                            <label>Role</label>
+                            <input type="text" class="form-control" id="role" value="Admin" readonly />
+                        </div>
+                        <div class="form-group">
+                            <label id="passwordLabel">Password</label>
+                            <input type="password" class="form-control" id="password" placeholder="************" readonly />
+                        </div>
+                    </form>
+
+                </div>
             </div>
         </div>
     );
