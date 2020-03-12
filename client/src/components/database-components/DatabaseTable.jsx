c9236533a538179f1cc04a069b1795881f76e75c
Jeffrey Fallis <jeffreyfallis@hotmail.com>
2020-03-11 17:45:27 -0700

    Added table headers

--- client/src/components/database-components/DatabaseTable.jsx
+++ client/src/components/database-components/DatabaseTable.jsx
@@ -3,9 +3,28 @@ import '../styles/Database.scss'
 
 const DatabaseTable = props => {
     return (
-        <div id="table" style={{marginTop: `${props.margin}px`}}>
-            <h1>{props.margin}</h1>
-        </div>
+        <div class="container-fluid p-0 pb-5" id="table" style={{marginTop: `${props.margin}px`}}>
+        <table class="table table-striped table-bordered table-hover ">
+          <thead class="thead-dark">
+            <tr>
+              <th>Name</th>
+              <th>Group</th>
+              <th>Measurement Technique</th>
+              <th>Major Elements (wt%)</th>
+              <th>Minor Elements (ppm)</th>
+              <th>Trace Elements (ppb)</th>
+              <th>Title</th>
+              <th>Authors</th>
+              <th>Pg.#</th> 
+              <th>Journal</th> 
+              <th>Volume</th>
+              <th>Year Published</th> 
+            </tr>
+          </thead>
+          <tbody>
+          </tbody>
+          </table>
+          </div>   
     );
 }
 
