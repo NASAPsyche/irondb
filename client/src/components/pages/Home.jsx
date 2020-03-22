569302bf26e78b10dd7e7a76a1b18beebe0edd38
Cody Brobston <cbrobston22@gmail.com>
2020-03-21 19:04:42 -0700

    Changed attributes to camelCase in Home.jsx.

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -44,11 +44,11 @@ const Home = () => {
                     <form id="home-search" action="/database" class="border border-dark p-3" onSubmit={handleSubmit}>
                         <div className="d-flex flex-row align-items-center justify-content-center ">
                             <div className="col-md-3">
-                                <label className="sr-only" for="name">Meteorite Name</label>
+                                <label className="sr-only" htmlFor ="name">Meteorite Name</label>
                                 <input type="text" name="name" id="name" class="form-control" placeholder="meteorite name" onChange={handleChangeName}/>
                             </div>
                             <div className="col-md-2">
-                                <label className="sr-only" for="group">group</label>
+                                <label className="sr-only" htmlFor ="group">group</label>
                                 <select className="form-control" id="group" name="group" placeholder="group" onChange={handleChangeGroup}>
                                     <option value="" disabled selected hidden>group</option>
                                     <option>IAB</option>
@@ -60,11 +60,11 @@ const Home = () => {
                                 </select>
                             </div>
                             <div className="col-md-2">
-                                <label className="sr-only" for="title">Paper Title</label>
+                                <label className="sr-only" htmlFor ="title">Paper Title</label>
                                 <input type="text" name="title" id="title" class="form-control" placeholder="paper title" onChange={handleChangeTitle}/>
                             </div>
                             <div className="col-md-2">
-                                <label className="sr-only" for="author">Author</label>
+                                <label className="sr-only" htmlFor ="author">Author</label>
                                 <input type="text" name="author" id="author" class="form-control" placeholder="author" onChange={handleChangeAuthor}/>
                             </div>
                             <div className="col-md-2 col-sm-3">
@@ -88,14 +88,14 @@ const Home = () => {
                     <p><small>This website and work were created in partial fulfillment of Arizona State University Capstone Course “SER 401-402.”
                         The work is a result of the Psyche Student Collaborations component of NASA’s Psyche Mission (<a href="https://psyche.asu.edu">https://psyche.asu.edu</a>).
                         “Psyche: A Journey to a Metal World” [Contract number NNM16AA09C] is part of the NASA Discovery Program mission to solar system targets.
-                        Trade names and trademarks of ASU and NASA are used in this website for identification only.
+                        Trade names and trademarks of ASU and NASA are used in this website htmlFor identification only.
                         Their usage does not constitute an official endorsement, either expressed or implied, by Arizona State University or National Aeronautics and Space Administration.
                         The content is solely the responsibility of the authors and does not necessarily represent the official views of ASU or NASA.
                     </small></p>
                 </div>
             </div>
         </body>
-    </div> 
+    </div>
     );
 }
 
