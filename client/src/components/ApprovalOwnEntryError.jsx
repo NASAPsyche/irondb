624e428d0cfea54067580d4245f3b940e64ca004
Cody Brobston <cbrobston22@gmail.com>
2020-03-21 19:20:34 -0700

    Fixed camelCase attributes in components folder.

--- client/src/components/ApprovalOwnEntryError.jsx
+++ client/src/components/ApprovalOwnEntryError.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 export default function ApprovalOwnEntryError() {
     return (
-        <div class="mt-6">
-            <div class="d-flex flex-row align-items-center justify-content-center">
+        <div className="mt-6">
+            <div className="d-flex flex-row align-items-center justify-content-center">
                 <h1>Can't approve own entries 🙅‍♀️</h1>
             </div>
-            <div class="d-flex flex-row align-items-center justify-content-center">
+            <div className="d-flex flex-row align-items-center justify-content-center">
                 {/* TODO: CREATE DATABASE/UNAPPROVED PAGE */}
-                <a href="/database/unapproved" class="btn btn-danger btn-lg mt-3">Back to unapproved</a>
+                <a href="/database/unapproved" className="btn btn-danger btn-lg mt-3">Back to unapproved</a>
             </div>
         </div>
     )
