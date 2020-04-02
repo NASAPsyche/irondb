8bc81898dd0dc199f9ae3f78309f54d81e5f6052
Jeffrey Fallis <jeffreyfallis@hotmail.com>
2020-04-01 17:00:08 -0700

    Added major, minor, and trace elements to the table

--- client/src/components/database-components/TableEntry.jsx
+++ client/src/components/database-components/TableEntry.jsx
@@ -1,18 +1,63 @@
 import React from "react";
 
-const TableEntry = props => {
-    console.log(props.object);
+const TableEntry = props => { 
     const {meteorite_name, classification_group, technique, title, authors, 
-        trace_elements, page_number, journal_name, volume, published_year} = props.object;
+        trace_elements, major_elements, minor_elements, page_number, journal_name, 
+        volume, published_year} = props.object;
+        console.log(trace_elements);
+
+    var major = "null";
+    var minor = "null";
+    var trace = "null";
+
+    if (major_elements !== null) {
+        major = "";
+    }
+
+    if (minor_elements !== null) {
+        minor = "";
+    }
+
+    if (trace_elements !== null) {
+        trace = "";
+        for (let i = 0; i < trace_elements.length; i++) {
+            
+            let symbol = ": ";
+            let value = trace_elements[i].split(",");
+            if (value[3] === "true") {
+                symbol += "< ";
+            }
+            trace += value[0].toUpperCase() + symbol + value[1] + " ";
+        }
+    } 
+
 
     return (
         <tr id="entry">
             <th>{meteorite_name}</th>
             <th>{classification_group}</th>
             <th>{technique}</th>
-            <th></th>
-            <th></th>
-            <th></th>
+            <th>{major_elements !== null ? major_elements.map(entry => {
+                let value = entry.split(",");
+                let element = value[0].charAt(0).toUpperCase() + value[0].substring(1);
+                let symbol = ": ";
+                if (value[3] == "true") {symbol += "< ";}
+                return <span>{element}{symbol}{value[1]}<br/></span>
+                }) : null}</th>
+            <th>{minor_elements !== null ? minor_elements.map(entry => {
+                let value = entry.split(",");
+                let element = value[0].charAt(0).toUpperCase() + value[0].substring(1);
+                let symbol = ": ";
+                if (value[3] == "true") {symbol += "< ";}
+                return <span>{element}{symbol}{value[1]}<br/></span>
+                }) : null}</th>
+            <th>{trace_elements !== null ? trace_elements.map(entry => {
+                let value = entry.split(",");
+                let element = value[0].charAt(0).toUpperCase() + value[0].substring(1);
+                let symbol = ": ";
+                if (value[3] == "true") {symbol += "< ";}
+                return <span>{element}{symbol}{value[1]}<br/></span>
+                }) : null}</th>
             <th>{title}</th>
             <th>{authors}</th>
             <th>{page_number}</th> 
